4de0377909325557c096d63bc49b88b8b639d786
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2013-07-18 10:21:37 +0200

    Replaced automatic setting of string-type Attribute type by explicit set of ENUM typed Attribute type

--- FIVES/Domainmodel/Attribute.cs
+++ FIVES/Domainmodel/Attribute.cs
@@ -4,26 +4,25 @@ namespace FIVES
 {
 	public class Attribute
 	{
-		public string type { get; set; }
+		public AttributeType type { get; set; }
 		public string value { get; set; }
 
 		public Attribute()
 		{
 		}
 
-		public Attribute(string type, string value)
+		public Attribute(AttributeType type, string value)
 		{
 			this.type = type;
 			this.value = value;
 		}
+	}
 
-		public void setValue<T>(T value)
-		{
-			string typeAsString = value.GetType().ToString();
-			string valueAsString = value.ToString();
-			this.type = typeAsString;
-			this.value = valueAsString;
-		}
+	public enum AttributeType {
+		INT,
+		FLOAT,
+		STRING,
+		BOOL
 	}
 }
 
