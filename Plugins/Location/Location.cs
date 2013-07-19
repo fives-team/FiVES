7b915b21ff4ada493063ba962cd59a124f579b94
Sergiy Byelozyorov <byelozyorov@cs.uni-saarland.de>
2013-07-19 19:47:56 +0200

    Created Location plugin stub

---
+++ Plugins/Location/Location.cs
@@ -0,0 +1,14 @@
+using System;
+using FIVES;
+
+namespace Location
+{
+    public class Location : IPluginInitializer
+    {
+        public Location()
+        {
+            // TODO: Register Location component in the component registry
+        }
+    }
+}
+
