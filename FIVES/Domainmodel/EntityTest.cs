171cd2ad2c6196001520dd9f00ddf42f0d7dc1c6
Sergiy Byelozyorov <byelozyorov@cs.uni-saarland.de>
2013-07-19 19:50:03 +0200

    Converted tabs to 4 spaces

--- FIVES/Domainmodel/EntityTest.cs
+++ FIVES/Domainmodel/EntityTest.cs
@@ -3,47 +3,47 @@ using NUnit.Framework;
 
 namespace FIVES
 {
-	[TestFixture()]
-	public class EntityTest
-	{
+    [TestFixture()]
+    public class EntityTest
+    {
 
-		public EntityTest ()
-		{
-		}
+        public EntityTest ()
+        {
+        }
 
-		[Test()]
-		public void ShouldAddOneChildren()
-		{
-			Entity parentEntity = new Entity ();
-			var EntityToAdd = new Entity ();
-			Assert.True(parentEntity.addChildNode (EntityToAdd));
-			Assert.False(parentEntity.addChildNode (EntityToAdd));
-			Assert.IsTrue (parentEntity.getAllChildren () [0] == EntityToAdd);
-			Assert.IsTrue (parentEntity.getAllChildren ().Count == 1);
-		}
+        [Test()]
+        public void ShouldAddOneChildren()
+        {
+            Entity parentEntity = new Entity ();
+            var EntityToAdd = new Entity ();
+            Assert.True(parentEntity.addChildNode (EntityToAdd));
+            Assert.False(parentEntity.addChildNode (EntityToAdd));
+            Assert.IsTrue (parentEntity.getAllChildren () [0] == EntityToAdd);
+            Assert.IsTrue (parentEntity.getAllChildren ().Count == 1);
+        }
 
-		[Test()]
-		public void ShouldCorrectlyAssignParent()
-		{
-			Entity parentEntity = new Entity ();
-			Entity childEntity = new Entity ();
-			parentEntity.addChildNode (childEntity);
-			Assert.IsTrue (childEntity.parent == parentEntity);
-		}
+        [Test()]
+        public void ShouldCorrectlyAssignParent()
+        {
+            Entity parentEntity = new Entity ();
+            Entity childEntity = new Entity ();
+            parentEntity.addChildNode (childEntity);
+            Assert.IsTrue (childEntity.parent == parentEntity);
+        }
 
-		[Test()]
-		public void ShouldCorrectlyRetrieveFirstAndLast()
-		{
-			Entity parentEntity = new Entity ();
-			Entity firstChildEntity = new Entity ();
-			Entity lastChildEntity = new Entity ();
+        [Test()]
+        public void ShouldCorrectlyRetrieveFirstAndLast()
+        {
+            Entity parentEntity = new Entity ();
+            Entity firstChildEntity = new Entity ();
+            Entity lastChildEntity = new Entity ();
 
-			parentEntity.addChildNode (firstChildEntity);
-			parentEntity.addChildNode (lastChildEntity);
+            parentEntity.addChildNode (firstChildEntity);
+            parentEntity.addChildNode (lastChildEntity);
 
-			Assert.IsTrue (parentEntity.getFirstChild () == firstChildEntity);
-			Assert.IsTrue (parentEntity.getLastChild () == lastChildEntity);
-		}
-	}
+            Assert.IsTrue (parentEntity.getFirstChild () == firstChildEntity);
+            Assert.IsTrue (parentEntity.getLastChild () == lastChildEntity);
+        }
+    }
 }
 
