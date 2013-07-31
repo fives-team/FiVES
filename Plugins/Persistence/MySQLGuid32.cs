540933ddc2d35cc38d09d6995162be888fadccd1
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2013-07-31 14:58:26 +0200

    Customized SQL Dialect to store GUIDs in same format as the application

--- Plugins/Persistence/MySQLGuid32.cs
+++ Plugins/Persistence/MySQLGuid32.cs
@@ -1,11 +1,14 @@
 using System;
+using System.Data;
+using NHibernate.Dialect;
 
 namespace Persistence
 {
-    public class MySQLGuid32
+    public class MySQLGuid32 :MySQL5Dialect
     {
         public MySQLGuid32 ()
         {
+            RegisterColumnType(DbType.Guid, "CHAR(36)");
         }
     }
 }
