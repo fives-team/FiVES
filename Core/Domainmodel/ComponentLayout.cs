b372dcaee69e9faeea61f5fd5dd6a884fdd7ab86
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2013-08-01 09:54:24 +0200

    Added mapping and employed necessary improvements to ComponentLayout

--- Core/Domainmodel/ComponentLayout.cs
+++ Core/Domainmodel/ComponentLayout.cs
@@ -19,7 +19,13 @@ namespace FIVES
 
         // We need to access this internally to be able to iterate over the list of the attributes when constructing a 
         // new component in ComponentRegistry::createComponent.
-        internal Dictionary<string, AttributeType> attributes = new Dictionary<string, AttributeType>();
+        internal IDictionary<string, AttributeType> attributes = new Dictionary<string, AttributeType>();
+        internal IDictionary<string, AttributeType> attributeHandler {
+            get { return this.attributes;}
+            set { this.attributes = value;}
+        }
+
+        private Guid Id {get; set; }
     }
 }
 
