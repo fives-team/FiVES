7c25efcd244c5818c5e769690c3acc15de946d8d
Sergiy Byelozyorov <byelozyorov@cs.uni-saarland.de>
2013-08-05 15:35:50 +0200

    Refactoring of PluginManager.
    isPluginLoaded now accepts plugin name instead of the file path
    isPathLoaded can be used to verify is a specific file was loaded

--- Core/PluginManager/PluginManager.cs
+++ Core/PluginManager/PluginManager.cs
@@ -24,42 +24,42 @@ namespace FIVES
 
         // Canoninizes the filename (converts .. and . into actual path). This allows to identify plugin from the same
         // file but different paths as the same. E.g. /foo/bar/baz/../plugin.dll is the same as /foo/bar/plugin.dll.
-        private string getCanonicalName(string filename)
+        private string getCanonicalPath(string filename)
         {
             return Path.GetFullPath(filename);
         }
 
-        // Attempts to load a plugin.
-        public void loadPlugin(string filename)
+        // Attempts to load a plugin from file located at |path|.
+        public void loadPlugin(string path)
         {
-            string canonicalName = getCanonicalName(filename);
-            if (!attemptedFilenames.Contains(canonicalName)) {
+            string canonicalPath = getCanonicalPath(path);
+            if (!attemptedFilenames.Contains(canonicalPath)) {
                 try {
                     // Add this plugin to the list of loaded paths.
-                    attemptedFilenames.Add(canonicalName);
+                    attemptedFilenames.Add(canonicalPath);
 
                     // Load an assembly.
-                    Assembly assembly = Assembly.LoadFrom(canonicalName);
+                    Assembly assembly = Assembly.LoadFrom(canonicalPath);
 
                     // Find initializer class.
                     List<Type> types = new List<Type>(assembly.GetTypes());
                     Type pluginInitializerInterface = typeof(IPluginInitializer);
                     Type initializerType = types.Find(t => pluginInitializerInterface.IsAssignableFrom(t));
                     if (initializerType == null) {
-                        logger.Warn("Assembly in file " + filename +
+                        logger.Warn("Assembly in file " + path +
                                     " doesn't contain any class implementing IPluginInitializer.");
                         return;
                     }
 
                     // Construct basic plugin info.
                     LoadedPluginInfo info = new LoadedPluginInfo();
-                    info.path = canonicalName;
+                    info.path = canonicalPath;
                     info.initializer = (IPluginInitializer)Activator.CreateInstance(initializerType);
 
                     // Check if plugin with the same name was already loaded.
                     string name = info.initializer.getName();
                     if (loadedPlugins.ContainsKey(name)) {
-                        logger.Warn("Cannot load plugin from " + filename + ". Plugin with the same name '" + name +
+                        logger.Warn("Cannot load plugin from " + path + ". Plugin with the same name '" + name +
                                     "' was already loaded from " + loadedPlugins[name].path + ".");
                         return;
                     }
@@ -79,7 +79,7 @@ namespace FIVES
                     // Initializes plugins that depend on current one.
                     initializeDeferredPlugins(name);
                 } catch (Exception e) {
-                    logger.WarnException("Failed to load file " + filename + " as a plugin.", e);
+                    logger.WarnException("Failed to load file " + path + " as a plugin.", e);
                     return;
                 }
             }
@@ -115,21 +115,27 @@ namespace FIVES
                 loadPlugin(filename);
         }
 
-        // Returns whether plugin in |filename| was loaded and initialized.
-        public bool isPluginLoaded(string filename)
+        // Returns whether plugin in |path| was loaded and initialized.
+        public bool isPathLoaded(string path)
         {
             // Check if we've attempted loading this filename before.
-            string canonicalName = getCanonicalName(filename);
-            if (!attemptedFilenames.Contains(canonicalName))
+            string canonicalPath = getCanonicalPath(path);
+            if (!attemptedFilenames.Contains(canonicalPath))
                 return false;
 
             // Check if the plugin was loaded.
             foreach (var plugin in loadedPlugins) {
-                if (plugin.Value.path == canonicalName)
+                if (plugin.Value.path == canonicalPath)
                     return true;
             }
 
             return false;
+        }
+
+        // Returns whether plugin with |name| is loaded.
+        public bool isPluginLoaded(string name)
+        {
+            return loadedPlugins.ContainsKey(name);
         }
     }
 }
--- Core/PluginManager/PluginManagerTest.cs
+++ Core/PluginManager/PluginManagerTest.cs
@@ -21,24 +21,27 @@ namespace FIVES
         public void shouldLoadAllValidPluginsInDirectory()
         {
             pm.loadPluginsFrom(pathToPlugins + "TestPlugins");
-            Assert.IsTrue(pm.isPluginLoaded(pathToPlugins + "TestPlugins/valid_plugin.dll"));
-            Assert.IsTrue(pm.isPluginLoaded(pathToPlugins + "TestPlugins/valid_plugin2.dll"));
+            Assert.IsTrue(pm.isPathLoaded(pathToPlugins + "TestPlugins/valid_plugin.dll"));
+            Assert.IsTrue(pm.isPathLoaded(pathToPlugins + "TestPlugins/valid_plugin2.dll"));
+            Assert.IsTrue(pm.isPluginLoaded("ValidPlugin1"));
+            Assert.IsTrue(pm.isPluginLoaded("ValidPlugin2"));
         }
 
         [Test()]
         public void shouldOmitInvalidPluginsInDirectory()
         {
             pm.loadPluginsFrom(pathToPlugins + "TestPlugins");
-            Assert.IsFalse(pm.isPluginLoaded(pathToPlugins + "TestPlugins/invalid_plugin.dll"));
-            Assert.IsFalse(pm.isPluginLoaded(pathToPlugins + "TestPlugins/invalid_assembly.dll"));
+            Assert.IsFalse(pm.isPathLoaded(pathToPlugins + "TestPlugins/invalid_plugin.dll"));
+            Assert.IsFalse(pm.isPathLoaded(pathToPlugins + "TestPlugins/invalid_assembly.dll"));
         }
 
         [Test()]
         public void shouldRecognizeDifferentPathsToTheSamePlugin()
         {
             pm.loadPlugin(pathToPlugins + "TestPlugins/valid_plugin.dll");
-            Assert.IsTrue(pm.isPluginLoaded(pathToPlugins + "TestPlugins/../TestPlugins/valid_plugin.dll"));
-            Assert.IsTrue(pm.isPluginLoaded(pathToPlugins + "TestPlugins/./valid_plugin.dll"));
+            Assert.IsTrue(pm.isPathLoaded(pathToPlugins + "TestPlugins/../TestPlugins/valid_plugin.dll"));
+            Assert.IsTrue(pm.isPathLoaded(pathToPlugins + "TestPlugins/./valid_plugin.dll"));
+            Assert.IsTrue(pm.isPluginLoaded("ValidPlugin1"));
         }
 
         [Test()]
@@ -46,7 +49,7 @@ namespace FIVES
         {
             var testPlugin = pathToPlugins + "TestPlugins/non-existing-file.dll";
             pm.loadPlugin(testPlugin);
-            Assert.IsFalse(pm.isPluginLoaded(testPlugin));
+            Assert.IsFalse(pm.isPathLoaded(testPlugin));
         }
 
         [Test()]
@@ -54,7 +57,7 @@ namespace FIVES
         {
             var testPlugin = pathToPlugins + "TestPlugins/invalid_plugin.dll";
             pm.loadPlugin(testPlugin);
-            Assert.IsFalse(pm.isPluginLoaded(testPlugin));
+            Assert.IsFalse(pm.isPathLoaded(testPlugin));
         }
 
         [Test()]
@@ -62,17 +65,20 @@ namespace FIVES
         {
             var testPlugin = pathToPlugins + "TestPlugins/invalid_assembly.dll";
             pm.loadPlugin(testPlugin);
-            Assert.IsFalse(pm.isPluginLoaded(testPlugin));
+            Assert.IsFalse(pm.isPathLoaded(testPlugin));
         }
 
         [Test()]
         public void shouldLoadPluginsInDependencyOrder()
         {
             pm.loadPlugin(pathToPlugins + "TestPlugins/valid_plugin2.dll");
-            Assert.False(pm.isPluginLoaded(pathToPlugins + "TestPlugins/valid_plugin2.dll"));
+            Assert.IsFalse(pm.isPathLoaded(pathToPlugins + "TestPlugins/valid_plugin2.dll"));
+            Assert.IsFalse(pm.isPluginLoaded("ValidPlugin2"));
             pm.loadPlugin(pathToPlugins + "TestPlugins/valid_plugin.dll");
-            Assert.True(pm.isPluginLoaded(pathToPlugins + "TestPlugins/valid_plugin.dll"));
-            Assert.True(pm.isPluginLoaded(pathToPlugins + "TestPlugins/valid_plugin2.dll"));
+            Assert.IsTrue(pm.isPathLoaded(pathToPlugins + "TestPlugins/valid_plugin.dll"));
+            Assert.IsTrue(pm.isPathLoaded(pathToPlugins + "TestPlugins/valid_plugin2.dll"));
+            Assert.IsTrue(pm.isPluginLoaded("ValidPlugin1"));
+            Assert.IsTrue(pm.isPluginLoaded("ValidPlugin2"));
         }
     }
 }
