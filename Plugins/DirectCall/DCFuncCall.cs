cc7477c6c23b8b6de524ab06993ca6123142a72f
Sergiy Byelozyorov <byelozyorov@cs.uni-saarland.de>
2013-08-05 12:57:23 +0200

    Added FuncCall implementation for direct-call

---
+++ Plugins/DirectCall/DCFuncCall.cs
@@ -0,0 +1,15 @@
+using System;
+using KIARA;
+
+namespace DirectCall
+{
+    public class DCFuncCall : FuncCallBase
+    {
+        protected override object convertResult(object result, Type type)
+        {
+            // No need to convert. This is a direct call.
+            return result;
+        }
+    }
+}
+
