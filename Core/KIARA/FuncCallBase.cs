edae3cd8f39d6d0793be1df24569866b268de9a5
Sergiy Byelozyorov <byelozyorov@cs.uni-saarland.de>
2013-08-05 12:54:57 +0200

    Moved out common protocol functionality into a base class
    Now we can reuse the code for other protocols, e.g. direct-call

---
+++ Core/KIARA/FuncCallBase.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Diagnostics;
+
+namespace KIARA
+{
+    public abstract class FuncCallBase : IFuncCall
+    {
+        #region IFuncCall implementation
+
+        public IFuncCall onSuccess<T>(Action<T> handler)
+        {
+            if (state == State.InProgress)
+                successHandlers.Add(handler);
+            else if (state == State.Success)
+                handler((T)convertResult(result, typeof(T)));
+            return this;
+        }
+
+        public IFuncCall onException(Action<Exception> handler)
+        {
+            if (state == State.InProgress)
+                exceptionHandlers.Add(handler);
+            else if (state == State.Exception)
+                handler((Exception)result);
+            return this;
+        }
+
+        public IFuncCall onError(Action<string> handler)
+        {
+            if (state == State.InProgress)
+                errorHandlers.Add(handler);
+            else if (state == State.Error)
+                handler((string)result);
+            return this;
+        }
+
+        public T wait<T>(int millisecondsTimeout = -1)
+        {
+            T result = default(T);
+            onSuccess<T>(delegate (T value) { result = value; });
+            wait(millisecondsTimeout);
+            return result;
+        }
+
+        public void wait(int millisecondsTimeout = -1)
+        {
+            if (millisecondsTimeout == -1)
+                callFinished.WaitOne();
+            else
+                callFinished.WaitOne(millisecondsTimeout);
+
+            if (state == State.Error)
+                throw new Error(ErrorCode.CONNECTION_ERROR, "Error during the call. Reason: " + (string)result);
+            else if (state == State.Exception)
+                throw (Exception)result;
+        }
+
+        #endregion
+
+        protected void handleSuccess(object retValue)
+        {
+            state = State.Success;
+            result = retValue;
+
+            foreach (var handler in successHandlers) {
+                Debug.Assert(handler.Method.GetParameters().Length == 1);
+                Type argType = handler.Method.GetParameters()[0].GetType();
+                handler.DynamicInvoke(convertResult(retValue, argType));
+            }
+
+            successHandlers.Clear();
+            callFinished.Set();
+        }
+
+        protected void handleException(Exception exception)
+        {
+            state = State.Exception;
+            result = exception;
+
+            foreach (var handler in exceptionHandlers)
+                handler(exception);
+
+            exceptionHandlers.Clear();
+            callFinished.Set();
+        }
+
+        protected void handleError(string reason)
+        {
+            state = State.Error;
+            result = reason;
+
+            foreach (var handler in errorHandlers)
+                handler(reason);
+
+            errorHandlers.Clear();
+            callFinished.Set();
+        }
+
+        // Subclasses need to override this method to convert result to the type by the user callback.
+        protected abstract object convertResult(object result, Type type);
+
+        protected List<Delegate> successHandlers = new List<Delegate>();
+        protected List<Action<Exception>> exceptionHandlers = new List<Action<Exception>>();
+        protected List<Action<string>> errorHandlers = new List<Action<string>>();
+        protected AutoResetEvent callFinished = new AutoResetEvent(false);
+
+        protected enum State { InProgress, Success, Exception, Error };
+        protected State state = State.InProgress;
+        protected object result = null;
+    }
+}
+
--- Plugins/WebSocketJSON/WSJFuncCall.cs
+++ Plugins/WebSocketJSON/WSJFuncCall.cs
@@ -8,107 +8,27 @@ using System.Diagnostics;
 
 namespace WebSocketJSON
 {
-    public class WSJFuncCall : IFuncCall
+    public class WSJFuncCall : FuncCallBase
     {
-        #region FuncCall implementation
-
-        public IFuncCall onSuccess<T>(Action<T> handler)
+        protected override object convertResult(object result, Type type)
         {
-            if (state == State.InProgress)
-                successHandlers.Add(handler);
-            else if (state == State.Success)
-                handler(((JToken)result).ToObject<T>());
-            return this;
+            return ((JToken)result).ToObject(type);
         }
-
-        public IFuncCall onException(Action<Exception> handler)
-        {
-            if (state == State.InProgress)
-                exceptionHandlers.Add(handler);
-            else if (state == State.Exception)
-                handler((Exception)result);
-            return this;
-        }
-
-        public IFuncCall onError(Action<string> handler)
-        {
-            if (state == State.InProgress)
-                errorHandlers.Add(handler);
-            else if (state == State.Error)
-                handler((string)result);
-            return this;
-        }
-
-        public T wait<T>(int millisecondsTimeout = -1)
-        {
-            T result = default(T);
-            onSuccess<T>(delegate (T value) { result = value; });
-            wait(millisecondsTimeout);
-            return result;
-        }
-
-        public void wait(int millisecondsTimeout = -1)
-        {
-            if (millisecondsTimeout == -1)
-                callFinished.WaitOne();
-            else
-                callFinished.WaitOne(millisecondsTimeout);
-
-            if (state == State.Error)
-                throw new Error(ErrorCode.CONNECTION_ERROR, "Error during the call. Reason: " + (string)result);
-            else if (state == State.Exception)
-                throw (Exception)result;
-        }
-
-        #endregion
 
         internal void handleSuccess(JToken retValue)
         {
-            state = State.Success;
-            result = retValue;
-
-            foreach (var handler in successHandlers) {
-                Debug.Assert(handler.Method.GetParameters().Length == 1);
-                Type argType = handler.Method.GetParameters()[0].GetType();
-                handler.DynamicInvoke(retValue.ToObject(argType));
-            }
-
-            successHandlers.Clear();
-            callFinished.Set();
+            base.handleSuccess((object)retValue);
         }
 
         internal void handleException(JToken exception)
         {
-            state = State.Exception;
-            result = new Exception(exception.ToString());
-
-            foreach (var handler in exceptionHandlers)
-                handler((Exception)result);
-
-            exceptionHandlers.Clear();
-            callFinished.Set();
+            base.handleException(new Exception(exception.ToString()));
         }
 
         internal void handleError(JToken error)
         {
-            state = State.Error;
-            result = error.ToString();
-
-            foreach (var handler in errorHandlers)
-                handler((string)result);
-
-            errorHandlers.Clear();
-            callFinished.Set();
+            base.handleError(error.ToString());
         }
-
-        private List<Delegate> successHandlers = new List<Delegate>();
-        private List<Action<Exception>> exceptionHandlers = new List<Action<Exception>>();
-        private List<Action<string>> errorHandlers = new List<Action<string>>();
-        private AutoResetEvent callFinished = new AutoResetEvent(false);
-
-        private enum State { InProgress, Success, Exception, Error };
-        private State state = State.InProgress;
-        private object result = null;
     }
     
 }
