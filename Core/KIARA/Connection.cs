6576e333ca432479dd095e8f81a1092678e68a6f
Sergiy Byelozyorov <byelozyorov@cs.uni-saarland.de>
2013-08-06 15:05:08 +0200

    Added tests for KIARA.Connection class

--- Core/KIARA/Connection.cs
+++ Core/KIARA/Connection.cs
@@ -6,35 +6,55 @@ using System.Net;
 
 namespace KIARA
 {
+    // This class represenents a connection to the remote end. Can be used to generate remote function wrappers and to
+    // register func implementations on the local end.
     public class Connection
     {
+        // Func wrapper delegate generated with generateFuncWrapper. Allows passing arbitrary arguments and returns an
+        // object representing the remote call.
         public delegate IFuncCall FuncWrapper(params object[] args);
 
-        public Connection(IProtocol aProtocol)
-        {
-            protocol = aProtocol;
-        }
+        // Constructs connection from a protocol. Please use Context.openConnection or Context.startServer to construct
+        // a new connection.
+        internal Connection(IProtocol aProtocol) : this(aProtocol, new WebClientWrapper()) {}
 
+        // Loads an IDL at |uri| into the connection.
         public void loadIDL(string uri)
         {
-            WebClient client = new WebClient();
-            string contents = client.DownloadString(uri);
+            string contents = webClient.DownloadString(uri);
             // TODO: Parse the IDL and pass parsed structure into processIDL.
             protocol.processIDL(contents);
         }
 
+        // Generates a func wrapper for the |funcName|. Optional |typeMapping| can be used to specify data omission and
+        // reordering options.
         public FuncWrapper generateFuncWrapper(string funcName, string typeMapping = "")
         {
+            // TODO: implement type mapping and add respective tests
             return (FuncWrapper) delegate(object[] args) {
                 return protocol.callFunc(funcName, args);
             };
         }
 
+        // Registers a local |handler| as an implementation for the |funcName|. Optional |typeMapping| can be used to
+        // specify data omission and reordering options.
         public void registerFuncImplementation(string funcName, Delegate handler, string typeMapping = "")
         {
+            // TODO: implement type mapping and add respective tests
             protocol.registerHandler(funcName, handler);
         }
 
-        IProtocol protocol;
+        private IProtocol protocol;
+        private IWebClient webClient;
+
+        #region Testing
+
+        internal Connection(IProtocol aProtocol, IWebClient client)
+        {
+            protocol = aProtocol;
+            webClient = client;
+        }
+
+        #endregion
     }
 }
---
+++ Core/KIARA/ConnectionTest.cs
@@ -0,0 +1,54 @@
+using System;
+using NUnit.Framework;
+using Moq;
+
+namespace KIARA
+{
+    [TestFixture()]
+    public class ConnectionTest
+    {
+        private string idlURL = "http://www.example.com/my.idl";
+
+        [Test()]
+        public void ShouldCallFuncInProtocol()
+        {
+            var mockProtocol = new Mock<IProtocol>();
+
+            Connection c = new Connection(mockProtocol.Object);
+            var testFunc = c.generateFuncWrapper("testFunc");
+            testFunc("testArg", 42);
+
+            mockProtocol.Verify(protocol => protocol.callFunc("testFunc", "testArg", 42), Times.Once());
+        }
+
+        [Test()]
+        public void ShouldDownloadIDLAndCallProcessIDLInProtocol()
+        {
+            var mockProtocol = new Mock<IProtocol>();
+            var mockWebClient = new Mock<IWebClient>();
+
+            mockWebClient.Setup(client => client.DownloadString(idlURL)).Returns("{}");
+
+            Connection c = new Connection(mockProtocol.Object, mockWebClient.Object);
+            c.loadIDL(idlURL);
+
+            mockWebClient.Verify(client => client.DownloadString(idlURL), Times.Once());
+            mockProtocol.Verify(protocol => protocol.processIDL("{}"), Times.Once());
+        }
+
+        [Test()]
+        public void ShouldCallRegisterHandlerInProtocol()
+        {
+            var mockProtocol = new Mock<IProtocol>();
+
+            Connection c = new Connection(mockProtocol.Object);
+            Delegate d = (Func<int, string>)delegate(int x) { return ""; };
+            c.registerFuncImplementation("testFunc", d);
+
+            mockProtocol.Verify(protocol => protocol.registerHandler("testFunc", d), Times.Once());
+        }
+
+        // TODO: Tests for type mapping
+    }
+}
+
