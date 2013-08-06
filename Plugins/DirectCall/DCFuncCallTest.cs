15e02fffe684c7a247d83e6d93850cd72ac80c40
Sergiy Byelozyorov <byelozyorov@cs.uni-saarland.de>
2013-08-06 22:57:31 +0200

    Restructured test

--- Plugins/DirectCall/DCFuncCallTest.cs
+++ Plugins/DirectCall/DCFuncCallTest.cs
@@ -5,26 +5,17 @@ using System;
 namespace DirectCall
 {
     [TestFixture()]
-    public class DCFuncCallTest
+    public class DCFuncCallTest : DCFuncCall
     {
-        private class DCFuncCallWrapper : DCFuncCall
-        {
-            public object testConvertResult(object result, Type type)
-            {
-                return base.convertResult(result, type);
-            }
-        }
-
         [Test()]
         public void shouldNotConvertResults()
         {
             int iValue = 42;
             float fValue = 3.14f;
             string sValue = "test";
-            DCFuncCallWrapper call = new DCFuncCallWrapper();
-            Assert.AreEqual(call.testConvertResult(iValue, typeof(string)), iValue);
-            Assert.AreEqual(call.testConvertResult(fValue, typeof(int)), fValue);
-            Assert.AreEqual(call.testConvertResult(sValue, typeof(float)), sValue);
+            Assert.AreEqual(base.convertResult(iValue, typeof(string)), iValue);
+            Assert.AreEqual(base.convertResult(fValue, typeof(int)), fValue);
+            Assert.AreEqual(base.convertResult(sValue, typeof(float)), sValue);
         }
     }
 }
