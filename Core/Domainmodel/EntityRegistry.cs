5e459e4318cc8da8ddaa1e6c883376f9c9382ea6
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2013-08-08 14:49:09 +0200

    Added Doxygen like comments to Entity registry

--- Core/Domainmodel/EntityRegistry.cs
+++ Core/Domainmodel/EntityRegistry.cs
@@ -4,12 +4,19 @@ using System.Collections.Generic;
 
 namespace FIVES
 {
-    // Manages entities in the database.
+    /// <summary>
+    /// Manages entities in the database
+    /// </summary>
     public class EntityRegistry
     {
         public readonly static EntityRegistry Instance = new EntityRegistry();
 
-        // Adds a new entity to the database. Returns GUID assigned to the entity.
+        /// <summary>
+        /// Adds a new entity without any Guid assigned yet to the database.
+        ///  A new Guid is generated for the entity and returned by the function
+        /// </summary>
+        /// <returns>Guid assigned to the new Entity</returns>
+        /// <param name="entity">Entity that is to be added to the registry</param>
         public Guid addEntity(Entity entity)
         {
             Guid newGUID = Guid.NewGuid();
@@ -18,10 +25,20 @@ namespace FIVES
             return newGUID;
         }
 
+        /// <summary>
+        /// Adds an entity that has already a Guid assigned to the registry.
+        /// </summary>
+        /// <param name="entity">Entity to be added to the registry</param>
+        public void addEntityWithGUID(Entity entity)
         {
             entities[entity.Guid] = entity;
         }
-        // Removes an entity with a given |guid|. Returns false if such entity was not found.
+
+        /// <summary>
+        ///  Removes an entity with a given <b>guid</b>. Returns false if such entity was not found.
+        /// </summary>
+        /// <returns><c>true</c>, if entity was removed, <c>false</c> otherwise.</returns>
+        /// <param name="guid">GUID of the entity that should be removed</param>
         public bool removeEntity(Guid guid)
         {
             if (entities.ContainsKey(guid)) {
@@ -32,7 +49,11 @@ namespace FIVES
             return false;
         }
 
-        // Returns entity by its |guid| or null if no such entity is found.
+        /// <summary>
+        /// Returns entity by its <b>guid</b> or null if no such entity is found.
+        /// </summary>
+        /// <returns>The entity by GUID.</returns>
+        /// <param name="guid">GUID.</param>
         public Entity getEntityByGuid(Guid guid)
         {
             if (entities.ContainsKey(guid))
@@ -40,7 +61,10 @@ namespace FIVES
             return null;
         }
 
-        // Returns a list of all entities' GUIDs.
+        /// <summary>
+        ///  Returns a list of all entities' GUIDs.
+        /// </summary>
+        /// <returns>The all GUI ds.</returns>
         public List<Guid> getAllGUIDs()
         {
             List<Guid> res = new List<Guid>();
