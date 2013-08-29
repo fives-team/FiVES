8ddef7bef5c810aa4737927e540c572c2de0e78d
Sergiy Byelozyorov <byelozyorov@cs.uni-saarland.de>
2013-08-29 10:29:59 +0200

    Implemented context construction. Fixed minor bugs. Added documentation.

--- Plugins/Scripting/IJavaScriptContext.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Scripting
-{
-    public interface IJavaScriptContext
-    {
-    }
-}
-
--- Plugins/Scripting/ScriptingPlugin.cs
+++ Plugins/Scripting/ScriptingPlugin.cs
@@ -2,23 +2,40 @@ using System;
 using FIVES;
 using System.Collections.Generic;
 using KIARA;
+using Events;
+using V8.Net;
 
 namespace Scripting
 {
+    /// <summary>
+    /// Scripting plugin.
+    /// </summary>
     public class ScriptingPlugin : IPluginInitializer
     {
         #region IPluginInitializer implementation
 
+        /// <summary>
+        /// Returns the name of the plugin.
+        /// </summary>
+        /// <returns>The name of the plugin.</returns>
         public string getName()
         {
             return "Scripting";
         }
 
+        /// <summary>
+        /// Returns the list of names of the plugins that this plugin depends on.
+        /// </summary>
+        /// <returns>The list of names of the plugins that this plugin depends on.</returns>
         public List<string> getDependencies()
         {
             return new List<string>();
         }
 
+        /// <summary>
+        /// Initializes the plugin. This method will be called by the plugin manager when all dependency plugins have
+        /// been loaded.
+        /// </summary>
         public void initialize()
         {
             // Provide scripting API to other plugins.
@@ -26,16 +43,61 @@ namespace Scripting
 
             // Register 'scripting' component.
             ComponentLayout layout = new ComponentLayout();
-            layout["ownerScript"] = AttributeType.STRING;
-            layout["serverScript"] = AttributeType.STRING;
-            layout["clientScript"] = AttributeType.STRING;
-            ComponentRegistry.Instance.defineComponent("scripting", pluginGUID, layout);
+            layout[ownerScriptAttributeName] = typeof(string);
+            layout[serverScriptAttributeName] = typeof(string);
+            layout[clientScriptAttributeName] = typeof(string);
+            ComponentRegistry.Instance.defineComponent(scriptingComponentName, pluginGUID, layout);
 
-            // TODO: register for changes to the scripting component via event system (once implemented)
-            // TODO: register for creation of new entities and parse their scripting component (if present)
+            EntityRegistry.Instance.OnEntityAdded += handleOnEntityAdded;
         }
 
         #endregion
+
+        private const string scriptingComponentName = "scripting";
+        private const string ownerScriptAttributeName = "ownerScript";
+        private const string serverScriptAttributeName = "serverScript";
+        private const string clientScriptAttributeName = "clientScript";
+
+        private void handleOnEntityAdded(object sender, EntityAddedOrRemovedEventArgs e)
+        {
+            Entity newEntity = EntityRegistry.Instance.getEntity(e.elementId);
+            // FIXME: This is not very efficient. Would be nice to be triggered only when anything in "scripting" 
+            // component is changed, but without the need to create one. Essentially we need an event 
+            // OnComponentCreated.
+            newEntity.OnAttributeInComponentChanged += delegate(object sender2, AttributeInComponentEventArgs e2) {
+                if (e2.componentName == scriptingComponentName)
+                    initEntityContext(newEntity);
+            };
+            initEntityContext(newEntity);
+        }
+
+
+        private void initEntityContext(Entity entity)
+        {
+            // Remove previous context if any.
+            entityContexts.Remove(entity.Guid);
+
+            string serverScript = (string)entity[scriptingComponentName][serverScriptAttributeName];
+            if (serverScript == null)
+                return;
+
+            var engine = new V8Engine();
+            entityContexts.Add(entity.Guid, engine);
+
+            // This object should be used to assign event handlers, e.g. script.onNewObject = function (newObject) {...}
+            var context = new V8NetContext(engine);
+            context.Execute("script = {}");
+
+            // Register global objects.
+            foreach (var entry in registeredGlobalObjects)
+                engine.GlobalObject.SetProperty(entry.Key, entry.Value, null, true, V8PropertyAttributes.Locked);
+
+            // Invoke new context handlers.
+            newContextHandlers.ForEach(handler => handler(context));
+
+            // Execute server script.
+            engine.Execute(serverScript);
+        }
 
         private void registerGlobalObject(string name, object csObject)
         {
@@ -44,7 +106,7 @@ namespace Scripting
             registeredGlobalObjects[name] = csObject;
         }
 
-        private void addNewContextHandler(Action<IJavaScriptContext> handler)
+        private void addNewContextHandler(Action<IJSContext> handler)
         {
             newContextHandlers.Add(handler);
         }
@@ -53,15 +115,17 @@ namespace Scripting
         {
             conn.registerFuncImplementation("registerGlobalObject", (Action<string, object>)registerGlobalObject);
             conn.registerFuncImplementation("addNewContextHandler",
-                                            (Action<Action<IJavaScriptContext>>)addNewContextHandler);
+                                            (Action<Action<IJSContext>>)addNewContextHandler);
         }
 
         private Dictionary<string, object> registeredGlobalObjects = new Dictionary<string, object>();
-        private List<Action<IJavaScriptContext>> newContextHandlers = new List<Action<IJavaScriptContext>>();
+        private List<Action<IJSContext>> newContextHandlers = new List<Action<IJSContext>>();
         private Context pluginContext = new Context();
 
+        private Dictionary<Guid, V8Engine> entityContexts = new Dictionary<Guid, V8Engine>();
+
         // {
-        //   'info': 'ClientSyncPlugin',
+        //   'info': 'ScriptingPlugin',
         //   'idlContent': '...',
         //   'servers': [{
         //     'services': '*',
@@ -71,9 +135,9 @@ namespace Scripting
         //     },
         //   }],
         // }
-        private readonly string pluginConfig = "data:text/json;base64,ew0KICAnaW5mbyc6ICdDbGllbnRTeW5jUGx1Z2luJywNCiA" +
-            "gJ2lkbENvbnRlbnQnOiAnLi4uJywNCiAgJ3NlcnZlcnMnOiBbew0KICAgICdzZXJ2aWNlcyc6ICcqJywNCiAgICAncHJvdG9jb2wnOiB" +
-            "7DQogICAgICAnbmFtZSc6ICdkaXJlY3QtY2FsbCcsDQogICAgICAnaWQnOiAnc2NyaXB0aW5nJywNCiAgICB9LA0KICB9XSwNCn0=";
+        private readonly string pluginConfig = "data:text/json;base64,ew0KICAnaW5mbyc6ICdTY3JpcHRpbmdQbHVnaW4nLA0KICA" +
+            "naWRsQ29udGVudCc6ICcuLi4nLA0KICAnc2VydmVycyc6IFt7DQogICAgJ3NlcnZpY2VzJzogJyonLA0KICAgICdwcm90b2NvbCc6IHs" +
+            "NCiAgICAgICduYW1lJzogJ2RpcmVjdC1jYWxsJywNCiAgICAgICdpZCc6ICdzY3JpcHRpbmcnLA0KICAgIH0sDQogIH1dLA0KfQ==";
 
         private readonly Guid pluginGUID = new Guid("90dd4c50-f09d-11e2-b778-0800200c9a66");
     }
