1b043360d12d51a35e8b4abf9dfb94a472b6af23
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2013-08-29 10:20:03 +0200

    Added documentation to persistence plugin

--- Plugins/Persistence/PersistencePlugin.cs
+++ Plugins/Persistence/PersistencePlugin.cs
@@ -15,16 +15,28 @@ namespace Persistence
 	{
         #region IPluginInitializer implementation
 
+        /// <summary>
+        /// Returns the name of the plugin.
+        /// </summary>
+        /// <returns>The name of the plugin.</returns>
         public string getName()
         {
             return "Persistence";
         }
 
+        /// <summary>
+        /// Returns the list of names of the plugins that this plugin depends on.
+        /// </summary>
+        /// <returns>The list of names of the plugins that this plugin depends on.</returns>
         public List<string> getDependencies()
         {
             return new List<string>();
         }
 
+        /// <summary>
+        /// Initializes the plugin. This method will be called by the plugin manager when all dependency plugins have
+        /// been loaded.
+        /// </summary>
         public void initialize()
         {
             initializeNHibernate ();
@@ -33,6 +45,10 @@ namespace Persistence
             EntityRegistry.Instance.OnEntityRemoved += onEntityRemoved;
         }
 
+        /// <summary>
+        /// Initializes NHibernate. Adds the mappings based on the assembly, initializes the session factory and opens
+        /// a long-running global session
+        /// </summary>
         private void initializeNHibernate() {
             nHibernateConfiguration.Configure ();
             nHibernateConfiguration.AddAssembly (typeof(Entity).Assembly);
@@ -40,6 +56,10 @@ namespace Persistence
             globalSession = sessionFactory.OpenSession ();
         }
 
+        /// <summary>
+        /// Initializes the state of the system as stored in the database by retrieving the stored component registry and
+        /// the stored entities
+        /// </summary>
         private void initializePersistedCollections() {
             retrieveComponentRegistryFromDatabase ();
             retrieveEntitiesFromDatabase ();
@@ -48,6 +68,12 @@ namespace Persistence
         #endregion
 
         #region Event Handlers
+        /// <summary>
+        /// Event Handler for EntityAdded event fired by the EntityRegistry. A new entity will be persisted to the database,
+        /// unless the entity to be added was just queried from the database (e.g. upon initialization)
+        /// </summary>
+        /// <param name="sender">Sender of the event (the EntityRegistry)</param>
+        /// <param name="e">Event arguments</param>
         internal void onEntityAdded(Object sender, EntityAddedOrRemovedEventArgs e) {
             Entity addedEntity = EntityRegistry.Instance.getEntity (e.elementId);
             addedEntity.OnAttributeInComponentChanged += onComponentChanged;
@@ -59,11 +85,24 @@ namespace Persistence
             }
         }
 
+        /// <summary>
+        /// Event Handler for EntityRemoved event fired by the entity registry. Once an entity is removed from the registry,
+        /// its entry is also deleted from the database (including cascaded deletion of its component instances)
+        /// </summary>
+        /// <param name="sender">Sender of the event (the EntityRegistry)</param>
+        /// <param name="e">Event Arguments</param>
         internal void onEntityRemoved(Object sender, EntityAddedOrRemovedEventArgs e) {
             Entity entityToRemove = EntityRegistry.Instance.getEntity (e.elementId);
             removeEntityFromDatabase (entityToRemove);
         }
 
+        /// <summary>
+        /// Event Handler for AttributeInComponentChanged on Entity. When fired by the entity, the entity is persisted to the
+        /// data base, with casacading save of components. The whole entity is persisted (instead of only the component), to
+        /// correctly store the link of the component to the entity on newly instantiated components as well.
+        /// </summary>
+        /// <param name="sender">Sender of the event (the Entity)</param>
+        /// <param name="e">Event arguments</param>
         internal void onComponentChanged(Object sender, AttributeInComponentEventArgs e) {
             Entity changedEntity = (Entity)sender;
             Component changedComponent = changedEntity [e.componentName];
@@ -74,6 +113,11 @@ namespace Persistence
         #endregion
 
         #region database synchronisation
+
+        /// <summary>
+        /// Persists an entity to database.
+        /// </summary>
+        /// <param name="addedEntity">Added entity</param>
         private void persistEntityToDatabase(Entity addedEntity) {
 
             using(ISession session = sessionFactory.OpenSession()) {
@@ -83,6 +127,10 @@ namespace Persistence
             }
         }
 
+        /// <summary>
+        /// Removes an entity from database.
+        /// </summary>
+        /// <param name="entity">Entity.</param>
         private void removeEntityFromDatabase(Entity entity) {
             using (ISession session = sessionFactory.OpenSession()) {
                 var transaction = session.BeginTransaction ();
@@ -90,6 +138,11 @@ namespace Persistence
                 transaction.Commit ();
             }
         }
+
+        /// <summary>
+        /// Persists the component to database.
+        /// </summary>
+        /// <param name="component">Component.</param>
         private void persistComponentToDatabase(Component component) {
             using(ISession session = sessionFactory.OpenSession()) {
                 var transaction = session.BeginTransaction ();
@@ -98,6 +151,9 @@ namespace Persistence
             }
         }
 
+        /// <summary>
+        /// Retrieves the component registry from database.
+        /// </summary>
         internal void retrieveComponentRegistryFromDatabase()
         {
             ComponentRegistryPersistence persistedRegistry = null;
@@ -108,6 +164,9 @@ namespace Persistence
 
         }
 
+        /// <summary>
+        /// Retrieves the entities from database.
+        /// </summary>
         internal void retrieveEntitiesFromDatabase()
         {
             IList<Entity> entitiesInDatabase = new List<Entity> ();
