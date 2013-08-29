0df77c030acc695ddfc44e1dfec8603abc9a49aa
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2013-08-29 12:10:23 +0200

    Added basic renderable plugin class

---
+++ Plugins/Renderable/RenderablePlugin.cs
@@ -0,0 +1,37 @@
+using System;
+using FIVES;
+using System.Collections.Generic;
+
+namespace Renderable
+{
+    public class RenderablePlugin : IPluginInitializer
+    {
+        #region IPluginInitializer implementation
+
+        public string getName()
+        {
+            return "Renderable";
+        }
+
+        public List<string> getDependencies()
+        {
+            return new List<string>();
+        }
+
+        public void initialize()
+        {
+            registerComponent ();
+        }
+
+        private void registerComponent()
+        {
+            ComponentLayout rendereableComponentLayout = new ComponentLayout ();
+            rendereableComponentLayout ["meshResource"] = typeof(string);
+            ComponentRegistry.Instance.defineComponent ("Renderable", this.pluginGUID, rendereableComponentLayout);
+        }
+
+        #endregion
+        private readonly Guid pluginGUID = new Guid("aff8cd50-3214-41cb-b13e-583bc7b7ef3a");
+    }
+}
+
