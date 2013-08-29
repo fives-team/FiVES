79ccc61f06fc363e42974e8b69db462e4e77b8b4
Sergiy Byelozyorov <byelozyorov@cs.uni-saarland.de>
2013-08-29 16:41:15 +0200

    Stopped creating new entities on each run. They are now persisted.

--- FIVES/FIVES.cs
+++ FIVES/FIVES.cs
@@ -22,22 +22,22 @@ namespace FIVES
                 logger.Error("Configuration is missing or corrupt.");
             }
 
-            // Add 5 entities.
-            var random = new Random();
-            for (int i = 0; i < 5; i++) {
-                dynamic entity = new Entity();
-                entity.position.x = (float)(random.NextDouble() * 100 - 50);
-                entity.position.y = (float)(random.NextDouble() * 100 - 50);
-                entity.position.z = (float)(random.NextDouble() * 100 - 50);
-
-                entity.scale.x = 1.0f;
-                entity.scale.y = 1.0f;
-                entity.scale.z = 1.0f;
-
-                entity.meshResource.uri = "my://path.to/resource";
-
-                EntityRegistry.Instance.addEntity(entity);
-            }
+//            // Add 5 entities.
+//            var random = new Random();
+//            for (int i = 0; i < 5; i++) {
+//                dynamic entity = new Entity();
+//                entity.position.x = (float)(random.NextDouble() * 100 - 50);
+//                entity.position.y = (float)(random.NextDouble() * 100 - 50);
+//                entity.position.z = (float)(random.NextDouble() * 100 - 50);
+//
+//                entity.scale.x = 1.0f;
+//                entity.scale.y = 1.0f;
+//                entity.scale.z = 1.0f;
+//
+//                entity.meshResource.uri = "my://path.to/resource";
+//
+//                EntityRegistry.Instance.addEntity(entity);
+//            }
 
             // Wait for any key to be pressed.
             Console.ReadKey();
