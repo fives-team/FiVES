a290271a53367d2dd25bd92fd056f3de7d99e510
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2013-09-16 09:12:09 +0200

    Added comments

--- Core/Domainmodel/Entity.cs
+++ Core/Domainmodel/Entity.cs
@@ -19,9 +19,17 @@ namespace FIVES
         private ComponentRegistry ComponentRegistry;
 
         public delegate void AttributeInComponentChanged (Object sender, AttributeInComponentEventArgs e);
+
+        /// <summary>
+        /// Occurs when the value of an attribute in one of the Entitys components has changed.
+        /// </summary>
         public event AttributeInComponentChanged OnAttributeInComponentChanged;
 
         public delegate void ComponentCreated (Object sender, ComponentCreatedEventArgs e);
+
+        /// <summary>
+        /// Occurs when lazy creation of one of the Entitys components has finished.
+        /// </summary>
         public event ComponentCreated OnComponentCreated;
 
         public Entity ()
@@ -34,6 +42,11 @@ namespace FIVES
 			Children = new List<Entity> ();
         }
 
+        /// <summary>
+        /// Adds a child node to the entity
+        /// </summary>
+        /// <returns><c>true</c>, if child node was added, <c>false</c> otherwise.</returns>
+        /// <param name="childEntity">Child entity.</param>
         public bool AddChildNode(Entity childEntity)
         {
             if (this.Children.Contains (childEntity))
@@ -47,6 +60,11 @@ namespace FIVES
             return true;
         }
 
+        /// <summary>
+        /// Removes a child node.
+        /// </summary>
+        /// <returns><c>true</c>, if child was removed, <c>false</c> otherwise.</returns>
+        /// <param name="entity">Entity to be removed.</param>
         public bool RemoveChild(Entity entity)
         {
             if (!this.Children.Contains (entity))
@@ -57,11 +75,19 @@ namespace FIVES
             return true;
         }
 
+        /// <summary>
+        /// returns all children.
+        /// </summary>
+        /// <returns>All children.</returns>
         public List<Entity> GetAllChildren()
         {
             return Children;
         }
 
+        /// <summary>
+        /// Gets the first child.
+        /// </summary>
+        /// <returns>The first child.</returns>
         public Entity GetFirstChild()
         {
             if(Children.Count == 0)
@@ -69,6 +95,10 @@ namespace FIVES
             return Children[0];
         }
 
+        /// <summary>
+        /// Gets the last child.
+        /// </summary>
+        /// <returns>The last child.</returns>
         public Entity GetLastChild()
         {
             if(Children.Count == 0)
@@ -76,6 +106,11 @@ namespace FIVES
             return Children [Children.Count -1];
         }
 
+        /// <summary>
+        /// Determines whether this instance has component with the specified name.
+        /// </summary>
+        /// <returns><c>true</c> if this instance has component with the specified name; otherwise, <c>false</c>.</returns>
+        /// <param name="name">Name.</param>
         public bool HasComponent(string name)
         {
             return this.Components.ContainsKey (name);
@@ -101,6 +136,10 @@ namespace FIVES
             }
         }
 
+        /// <summary>
+        /// Lazy instantiation of new component when component of specified name is accessed for the first time.
+        /// </summary>
+        /// <param name="componentName">Component name.</param>
         private void InstantiateNewComponent(string componentName) {
             Component newComponent = ComponentRegistry.GetComponentInstance (componentName);
             newComponent.OnAttributeChanged += delegate(object sender, AttributeChangedEventArgs e) {
