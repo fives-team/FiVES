49d80a1b61bb3871e08e6e473ed5fda19e1657d4
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2013-09-18 12:55:43 +0200

    Removed functions from app which are now done by entity creator

--- TestClient/scripts/app.js
+++ TestClient/scripts/app.js
@@ -9,41 +9,11 @@ require.config({
 requirejs(['kiara', 'jquery', 'websocket-json'],
 function(KIARA, $) {
 
-    function promptFloat(msg) {
-        var value;
-        do {
-            value = prompt(msg);
-        } while (isNaN(parseFloat(value)) || !isFinite(value))
-        return parseFloat(value);
-    }
-
-    function promptString(msg) {
-        var value;
-        do {
-            value = prompt(msg);
-        } while (value.toString() == "")
-        return value.toString();
-    }
-
-    function createNewEntity() {
-        var x = promptFloat("x = ");
-        var y = promptFloat("y = ");
-        var z = promptFloat("z = ");
-        FIVES.Communication.FivesCommunicator.createEntityAt(x, y, z);
-    }
-
     function setScript(guid) {
         //var script = promptString("serverScript = ");
         var script = "console.log('hello from client');";
         createServerScriptFor(guid, script);
         return false;
-    }
-
-    function addButton() {
-        var createButton = document.createElement("button");
-        createButton.appendChild(document.createTextNode("Create entity"));
-        createButton.addEventListener("click", createNewEntity);
-        document.body.appendChild(createButton);
     }
 
     function main() {
