64d89d516f4ab261aa837afe211ce5713514189e
Sergiy Byelozyorov <byelozyorov@cs.uni-saarland.de>
2013-09-20 14:57:27 +0200

    Removed unnecessary code (was already commented)

--- FIVES/FIVES.cs
+++ FIVES/FIVES.cs
@@ -36,23 +36,6 @@ namespace FIVES
 
             logger.Info("Loading complete");
 
-//            // Add 5 entities.
-//            var random = new Random();
-//            for (int i = 0; i < 5; i++) {
-//                var entity = new Entity();
-//                entity["position"]["x"] = (float)(random.NextDouble() * 100 - 50);
-//                entity["position"]["y"] = (float)(random.NextDouble() * 100 - 50);
-//                entity["position"]["z"] = (float)(random.NextDouble() * 100 - 50);
-//
-//                entity["scale"]["x"] = 1.0f;
-//                entity["scale"]["y"] = 1.0f;
-//                entity["scale"]["z"] = 1.0f;
-//
-//                entity["meshResource"]["uri"] = "my://path.to/resource";
-//
-//                EntityRegistry.Instance.addEntity(entity);
-//            }
-
             Console.WriteLine("The server is up and running. Press any key to stop it...");
 
             // Wait for any key to be pressed.
