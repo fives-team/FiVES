74ca2fc5735244a642848301f7c62841e5f4e21a
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2013-09-23 16:23:02 +0200

    Using connection member of communicator instead of passing parameter

--- TestClient/scripts/communication/fives_communicator.js
+++ TestClient/scripts/communication/fives_communicator.js
@@ -116,13 +116,13 @@ FIVES.Communication = FIVES.Communication || {};
     };
 
     var _createFunctionWrappers = function(error, supported) {
-            this.listObjects = connection.generateFuncWrapper("objectsync.listObjects");
-            this.getObjectLocation = connection.generateFuncWrapper("objectsync.getObjectLocation");
-            this.createEntityAt = connection.generateFuncWrapper("editing.createEntityAt");
-            this.createServerScriptFor = connection.generateFuncWrapper("scripting.createServerScriptFor");
-            this.notifyAboutNewObjects = connection.generateFuncWrapper("objectsync.notifyAboutNewObjects");
-            this.getObjectMesh = connection.generateFuncWrapper("objectsync.getObjectMesh");
-            this.setEntityOrientation = connection.generateFuncWrapper("objectsync.setEntityOrientation")
+            this.listObjects = this.connection.generateFuncWrapper("objectsync.listObjects");
+            this.getObjectLocation = this.connection.generateFuncWrapper("objectsync.getObjectLocation");
+            this.createEntityAt = this.connection.generateFuncWrapper("editing.createEntityAt");
+            this.createServerScriptFor = this.connection.generateFuncWrapper("scripting.createServerScriptFor");
+            this.notifyAboutNewObjects = this.connection.generateFuncWrapper("objectsync.notifyAboutNewObjects");
+            this.getObjectMesh = this.connection.generateFuncWrapper("objectsync.getObjectMesh");
+            this.setEntityOrientation = this.connection.generateFuncWrapper("objectsync.setEntityOrientation")
             this.notifyAboutNewObjects(FIVES.Models.EntityRegistry.addEntityFromServer);
             this.listObjects().on("result", _listObjectsCallback.bind(this));
     };
