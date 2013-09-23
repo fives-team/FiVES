b56c3c9dbe8b45fd4901505047a233d0eb92b377
Sergiy Byelozyorov <byelozyorov@cs.uni-saarland.de>
2013-09-23 15:22:45 +0200

    Added test for Renderable plugin

--- Plugins/Renderable/RenderablePluginTest.cs
+++ Plugins/Renderable/RenderablePluginTest.cs
@@ -1,14 +1,42 @@
 using NUnit.Framework;
 using System;
+using FIVES;
 
 namespace Renderable
 {
     [TestFixture()]
     public class RenderablePluginTest
     {
-        [Test()]
-        public void TestCase()
+        RenderablePlugin plugin = new RenderablePlugin();
+
+        public RenderablePluginTest()
         {
+            plugin.Initialize();
+        }
+
+        [Test()]
+        public void ShouldRegisterPositionComponent()
+        {
+            Assert.True(ComponentRegistry.Instance.IsRegistered("meshResource"));
+        }
+
+        [Test()]
+        public void ShouldRegisterOrientationComponent()
+        {
+            Assert.True(ComponentRegistry.Instance.IsRegistered("scale"));
+        }
+
+        [Test()]
+        public void ShouldReturnCorrectName()
+        {
+            Assert.AreEqual("Renderable", plugin.GetName());
+        }
+
+        [Test()]
+        public void ShouldReturnCorrectDeps()
+        {
+            Assert.AreEqual(plugin.GetDependencies().Count, 1);
+            Assert.Contains("Location", plugin.GetDependencies());
         }
     }
 }
