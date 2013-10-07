53abc6d94a09809f8cae92dca43a5361e34d8e4a
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2013-10-07 14:20:28 +0200

    Added comments for documentation

--- Plugins/ClientManager/ClientUpdateQueue.cs
+++ Plugins/ClientManager/ClientUpdateQueue.cs
@@ -6,6 +6,9 @@ using System.Threading;
 
 namespace ClientManager
 {
+    /// <summary>
+    /// Implements an Update queue that queues attribute changes of entities and sends those to clients to synchronize with the server state
+    /// </summary>
     internal class ClientUpdateQueue
     {
         internal struct UpdateInfo
@@ -17,6 +20,11 @@ namespace ClientManager
             public object value;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ClientManager.ClientUpdateQueue"/> class.
+        /// </summary>
+        /// <param name="clientGuid">GUID (session token) of client for which the queue is created</param>
+        /// <param name="callback">Callback provided by the client to be called in each update step</param>
         internal ClientUpdateQueue(string clientGuid, Action<List <UpdateInfo>> callback)
         {
             ClientCallback = callback;
@@ -24,6 +32,9 @@ namespace ClientManager
             RegisterToEntityUpdates();
         }
 
+        /// <summary>
+        /// Flushs the update queue. Takes the list of all updates queued for the client and calls the client's callback, passing this list as parameter
+        /// </summary>
         private void flushUpdateQueue() {
             while (!ClientDisconnected)
             {
@@ -38,23 +49,36 @@ namespace ClientManager
                     UpdateQueue.Clear();
                     Monitor.PulseAll(QueueLock);
                 }
+                Thread.Sleep(10); // Wait shortly to collect a number of atomic attribute updates before sending them for performance reasons
             }
         }
 
+        /// <summary>
+        /// Stops the client updates.
+        /// </summary>
         internal void StopClientUpdates() {
             ClientDisconnected = true;
         }
 
+        /// <summary>
+        /// Starts the update thread that performs the update loop.
+        /// </summary>
         private void StartUpdateThread () {
             ThreadPool.QueueUserWorkItem(_ => flushUpdateQueue());
         }
 
+        /// <summary>
+        /// Registers to Attribute Update events on Entities, as well as Entity Added or Removed of EntityRegistry
+        /// </summary>
         private void RegisterToEntityUpdates() {
             RegisterToExistingEntityUpdates();
             RegisterToAddedEntitiesUpdates();
             RegisterToEntityRemoved();
         }
 
+        /// <summary>
+        /// Subscribes to Attribute updates of all entities that were already in the scene when the client connected
+        /// </summary>
         private void RegisterToExistingEntityUpdates() {
             var entityGuids = EntityRegistry.Instance.GetAllGUIDs();
             foreach (Guid guid in entityGuids)
@@ -64,6 +88,10 @@ namespace ClientManager
             }
         }
 
+        /// <summary>
+        /// Registers to entity registry's OnEntityAdded to register on attribute updates of entities that are
+        /// created after the client has connected
+        /// </summary>
         private void RegisterToAddedEntitiesUpdates () {
             EntityRegistry.Instance.OnEntityAdded += (object sender, EntityAddedOrRemovedEventArgs e) => {
                 Entity newEntity = EntityRegistry.Instance.GetEntity(e.elementId);
@@ -71,12 +99,20 @@ namespace ClientManager
             };
         }
 
+        /// <summary>
+        /// Registers to entity removed event of entity registry to remove all entity updates of the removed entity that are still in the queue
+        /// </summary>
         private void RegisterToEntityRemoved () {
             EntityRegistry.Instance.OnEntityRemoved += (object sender, EntityAddedOrRemovedEventArgs e) => {
                 RemoveEntityFromQueue(e.elementId);
             };
         }
 
+        /// <summary>
+        /// Adds update information about an entity to the update queue after having received an attributeChanged event
+        /// </summary>
+        /// <param name="sender">Entity that invoked the attribute change event</param>
+        /// <param name="e">Event Arguments</param>
         private void AddEntityUpdateToQueue(Object sender, AttributeInComponentEventArgs e) {
 
             lock (QueueLock)
@@ -91,6 +127,12 @@ namespace ClientManager
             }
         }
 
+        /// <summary>
+        /// Creates the update info from event arguments.
+        /// </summary>
+        /// <returns>The update info from event arguments.</returns>
+        /// <param name="entityGuid">GUID of the entity triggering the AttributeChanged event</param>
+        /// <param name="e">Event arguments</param>
         private UpdateInfo createUpdateInfoFromEventArgs(Guid entityGuid, AttributeInComponentEventArgs e) {
             UpdateInfo newUpdateInfo = new UpdateInfo();
             newUpdateInfo.entityGuid = entityGuid.ToString();
@@ -100,6 +142,10 @@ namespace ClientManager
             return newUpdateInfo;
         }
 
+        /// <summary>
+        /// Removes all unsent update information of a removed entity from the update queue
+        /// </summary>
+        /// <param name="entityGuid">Guid of the removed entity/param>
         private void RemoveEntityFromQueue(Guid entityGuid) {
             lock (QueueLock)
             {
@@ -113,9 +159,24 @@ namespace ClientManager
             }
         }
 
+        /// <summary>
+        /// Indicates whether the client for which the queue was created has already disconnected
+        /// </summary>
         private volatile bool ClientDisconnected = false;
+
+        /// <summary>
+        /// Callback to be called on updates, provided by the client
+        /// </summary>
         private Action<List<UpdateInfo>> ClientCallback;
+
+        /// <summary>
+        /// Mutex Object for the update queue
+        /// </summary>
         private object QueueLock = new object();
+
+        /// <summary>
+        /// The update queue.
+        /// </summary>
         private List<UpdateInfo> UpdateQueue = new List<UpdateInfo>();
     }
 }
