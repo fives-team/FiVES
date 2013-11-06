ffa269827c7a8bcde5e4d2b07964f331bd9e6b61
Sergiy Byelozyorov <byelozyorov@cs.uni-saarland.de>
2013-11-06 21:07:04 +0100

    Finished renaming Utils into Collections

--- Core/Collections/ReadOnlyDictionary.cs
+++ Core/Collections/ReadOnlyDictionary.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Utils
+namespace Collections
 {
     public class ReadOnlyDictionary<TKey, TValue> : IDictionary<TKey, TValue>
     {
