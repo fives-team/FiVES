589e360e175974e12286cc4067620cdca39bed50
Sergiy Byelozyorov <byelozyorov@cs.uni-saarland.de>
2013-11-08 21:04:01 +0100

    Added test plugin projects into Misc

---
+++ Misc/TestPlugins/ValidPlugin1/ValidPlugin1Initializer.cs
@@ -0,0 +1,25 @@
+﻿using FIVES;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ValidPlugin1
+{
+    public class ValidPlugin1Initializer : IPluginInitializer
+    {
+        public string GetName()
+        {
+            return "ValidPlugin1";
+        }
+
+        public List<string> GetDependencies()
+        {
+            return new List<string>();
+        }
+
+        public void Initialize()
+        {
+        }
+    }
+}
