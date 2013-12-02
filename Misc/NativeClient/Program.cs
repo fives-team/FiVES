63fd4660567568bd788a8f8e5a811f6f3546a1ee
Sergiy Byelozyorov <byelozyorov@cs.uni-saarland.de>
2013-12-02 15:38:27 +0100

    Added support for "--logfile" parameter to NativeClient

--- Misc/NativeClient/Program.cs
+++ Misc/NativeClient/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using NLog;
 using System.Configuration;
+using NLog.Targets;
 
 namespace NativeClient
 {
@@ -10,6 +11,8 @@ namespace NativeClient
 
         public static void Main(string[] args)
         {
+            ConfigureNLog(args);
+
             var clientDriver = new ClientDriver();
 
             Logger.Info("Reading configuration");
@@ -21,9 +24,35 @@ namespace NativeClient
 
             clientDriver.StartSimulation();
 
-            Console.WriteLine("Client is running. Please any key to quit...");
-            Console.ReadKey();
+            // Wait for 'q' key to be pressed.
+            Console.WriteLine("The server is up and running. Press 'q' to stop it...");
+            while (Console.ReadKey().KeyChar != 'q');
             Environment.Exit(0);
+        }
+
+        /// <summary>
+        /// Configures NLog based on command line arguments.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        static void ConfigureNLog(string[] args)
+        {
+            string loggingFile = null;
+            foreach (string arg in args)
+            {
+                if (arg.StartsWith("--logfile="))
+                    loggingFile = arg.Substring(10);
+            }
+
+            if (loggingFile != null)
+            {
+                var fileTarget = LogManager.Configuration.FindTargetByName("logfile") as FileTarget;
+                if (fileTarget != null)
+                    fileTarget.FileName = loggingFile;
+            }
+            else
+            {
+                LogManager.Configuration.RemoveTarget("logfile");
+            }
         }
     }
 }
