ed00628085d02c35146e90bacaedc32e23693d31
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2013-12-11 16:22:53 +0100

    Fixed wrong namespace of PluginIntializer Class of Eventloop

--- Plugins/Eventloop/EventloopPluginInitializer.cs
+++ Plugins/Eventloop/EventloopPluginInitializer.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using FIVES;
 
-namespace Eventloop
+namespace EventloopPlugin
 {
     public class EventloopPluginInitializer : IPluginInitializer
     {
