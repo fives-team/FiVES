c3f88ea7e60b4305e40c2335beb37d22f989328b
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2013-12-11 16:21:02 +0100

    Added TickEvent Args which will be used by the periodically fired event

---
+++ Plugins/Eventloop/TickEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventloopPlugin
+{
+    class TickEventArgs : EventArgs
+    {
+        public TickEventArgs(TimeSpan timespan)
+        {
+            Timespan = timespan;
+        }
+        public TimeSpan Timespan { get; private set; }
+    }
+}
