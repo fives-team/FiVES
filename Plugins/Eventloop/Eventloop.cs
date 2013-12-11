ad4dc55b14ec089f53d43f8821b816122d291f26
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2013-12-11 16:19:55 +0100

    Added Eventloop Class that performs the actual loop

---
+++ Plugins/Eventloop/Eventloop.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventloopPlugin
+{
+    /// <summary>
+    /// Implements a simple event loop to which plugins can register. The Loop fires its event in a fixed interval that can be specified
+    /// in the config file. The EventArgs with which the event is invoked contain the time in milliseconds since the application is running
+    /// </summary>
+    public class Eventloop
+    {
+        public static Eventloop Instance;
+
+        /// <summary>
+        /// Event Handler to which other plugins can register
+        /// </summary>
+        public event EventHandler TickFired;
+
+        public Eventloop()
+        {
+        }
+    }
+}
