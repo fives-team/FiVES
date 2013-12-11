76db92c782761ab3f69cb01bc1e45738c24a4678
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2013-12-11 15:22:11 +0100

    Added Eventloop-Plugin project

---
+++ Plugins/Eventloop/EventloopPluginInitializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FIVES;
+
+namespace Eventloop
+{
+    public class EventloopPluginInitializer : IPluginInitializer
+    {
+        public string Name
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public List<string> PluginDependencies
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public List<string> ComponentDependencies
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
