7c6e59372ba9ba07fca0c4203c1680a43c136f47
Sergiy Byelozyorov <byelozyorov@cs.uni-saarland.de>
2013-12-16 15:50:52 +0100

    Implemented binary protocol based on raw TCP clients

---
+++ Protocols/BinaryProtocol/BPConnectionFactory.cs
@@ -0,0 +1,89 @@
+﻿using KIARAPlugin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using WebSocketJSON;
+
+namespace BinaryProtocol
+{
+    public class BPConnectionFactory : IConnectionFactory
+    {
+        public void OpenConnection(Server serverConfig, Context context, Action<Connection> onConnected)
+        {
+            ValidateProtocolName(serverConfig);
+
+            int port = ProtocolUtils.retrieveProtocolSetting(serverConfig, "port", -1);
+            string host = ProtocolUtils.retrieveProtocolSetting(serverConfig, "host", (string)null);
+
+            if (port == -1 || host == null)
+                throw new Error(ErrorCode.CONNECTION_ERROR, "No port and/or IP address is present in configuration.");
+
+            var socketAdapter = new BPSocketAdapter(host, port);
+            onConnected(new WSJConnection(socketAdapter));
+            socketAdapter.Open();
+        }
+
+        public void StartServer(Server serverConfig, Context context, Action<Connection> onNewClient)
+        {
+            ValidateProtocolName(serverConfig);
+
+            int port = ProtocolUtils.retrieveProtocolSetting(serverConfig, "port", 34838);
+            string host = ProtocolUtils.retrieveProtocolSetting(serverConfig, "host", "Any");
+
+            IPAddress localaddr;
+
+            if (host != "Any")
+            {
+                IPAddress[] ipAddresses = Dns.GetHostAddresses(host);
+                if (ipAddresses.Length == 0)
+                    throw new Error(ErrorCode.CONNECTION_ERROR, "Cannot identify IP address by hostname.");
+                localaddr = ipAddresses[0];  // we take first entry as it does not matter which one is used
+            }
+            else
+            {
+                localaddr = IPAddress.Any;
+            }
+
+            // Create connection listener.
+            TcpListener listener = new TcpListener(localaddr, port);
+            listener.Start();
+
+            // Wait for the first connection asynchronously.
+            var serverState = new AsyncServerState { Listener = listener, Callback = onNewClient };
+            listener.BeginAcceptTcpClient(CreateBPConnection, serverState);
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "binary-protocol";
+            }
+        }
+
+        private void CreateBPConnection(IAsyncResult ar)
+        {
+            AsyncServerState serverState = ar.AsyncState as AsyncServerState;
+            if (serverState == null)
+                throw new ArgumentException("Invalid async state in async result", "ar");
+            
+            // Create new connection and invoke the user callback.
+            TcpClient client = serverState.Listener.EndAcceptTcpClient(ar);
+            var socketAdapter = new BPSocketAdapter(client);
+            serverState.Callback(new WSJConnection(socketAdapter));
+
+            // Wait for the next connection attempt.
+            serverState.Listener.BeginAcceptTcpClient(CreateBPConnection, serverState);
+        }
+
+        private void ValidateProtocolName(Server serverConfig)
+        {
+            string protocol = ProtocolUtils.retrieveProtocolSetting<string>(serverConfig, "name", null);
+            if (protocol != "binary-protocol")
+                throw new Error(ErrorCode.CONNECTION_ERROR, "Given сonfig is not for binary-protocol protocol.");
+        }
+    }
+}
---
+++ Protocols/BinaryProtocol/BPSocketAdapter.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WebSocketJSON;
+
+namespace BinaryProtocol
+{
+    class BPSocketAdapter : ISocket
+    {
+        public BPSocketAdapter(string aHost, int aPort)
+        {
+            host = aHost;
+            port = aPort;
+        }
+
+        public BPSocketAdapter(TcpClient connectedClient)
+        {
+            client = connectedClient;
+            SetUpBufferAndStartReading();
+        }
+
+        public event EventHandler Opened;
+
+        public event EventHandler Closed;
+
+        // ???
+        public event EventHandler<ErrorEventArgs> Error;
+
+        public event EventHandler<MessageEventArgs> Message;
+
+        public void Open()
+        {
+            client = new TcpClient();
+            client.BeginConnect(host, port, HandleConnected, null);
+        }
+
+        public void Close()
+        {
+            client.Close();
+        }
+
+        public void Send(string message)
+        {
+            if (client.Connected)
+            {
+                // Each message is preceded by the 4-byte length and the content is encoded using UTF8.
+                byte[] msg = System.Text.Encoding.UTF8.GetBytes(message);
+                byte[] msgLength = BitConverter.GetBytes(msg.Length);
+                stream.Write(msgLength, 0, msgLength.Length);
+                stream.Write(msg, 0, msg.Length);
+            }
+            else
+            {
+                HandleNotConnected();
+            }
+        }
+
+        private void HandleNotConnected()
+        {
+            int wasOpened = Interlocked.CompareExchange(ref isOpened, 0, 1);
+            if (wasOpened == 1)
+            {
+                if (Closed != null)
+                    Closed(this, new EventArgs());
+            }
+            else
+            {
+                if (Error != null) 
+                {
+                    var e = new Exception("Failed to send. Socket is not yet opened or have been closed already");
+                    Error(this, new ErrorEventArgs(e));
+                }
+            }
+        }
+
+        private void HandleConnected(IAsyncResult ar)
+        {
+            client.EndConnect(ar);
+
+            SetUpBufferAndStartReading();
+
+            if (Opened != null)
+                Opened(this, new EventArgs());
+        }
+
+        private void SetUpBufferAndStartReading()
+        {
+            receiveBuffer = new byte[client.ReceiveBufferSize];
+            stream = client.GetStream();
+            stream.BeginRead(receiveBuffer, 0, receiveBuffer.Length, HandleFinishedRead, null);
+        }
+
+        private void HandleFinishedRead(IAsyncResult ar)
+        {
+            int readBytes = stream.EndRead(ar);
+            int offset = 0;
+
+            while (offset < readBytes)
+            {
+                int availableBytes = readBytes - offset;
+                if (incompleteMessage == null)
+                {
+                    if (incompleteMessageSize == null)
+                    {
+                        incompleteMessage = new byte[sizeof(int)];
+                        messageSizeCompletedBytes = 0;
+                    }
+
+                    if (availableBytes + messageSizeCompletedBytes >= incompleteMessageSize.Length)
+                    {
+                        Buffer.BlockCopy(receiveBuffer, offset, incompleteMessageSize, messageSizeCompletedBytes,
+                            incompleteMessageSize.Length - messageSizeCompletedBytes);
+                        offset += incompleteMessageSize.Length - messageSizeCompletedBytes;
+                        int messageSize = BitConverter.ToInt32(incompleteMessageSize, 0);
+                        incompleteMessageSize = null;
+                        incompleteMessage = new byte[messageSize];
+                        messageCompletedBytes = 0;
+                    }
+                    else
+                    {
+                        Buffer.BlockCopy(receiveBuffer, offset, incompleteMessageSize, messageSizeCompletedBytes,
+                            availableBytes);
+                        messageSizeCompletedBytes += availableBytes;
+                        offset += availableBytes;
+                    }
+                }
+                else
+                {
+                    if (availableBytes + messageCompletedBytes >= incompleteMessage.Length)
+                    {
+                        Buffer.BlockCopy(receiveBuffer, offset, incompleteMessage, messageCompletedBytes,
+                            incompleteMessage.Length - messageCompletedBytes);
+                        offset += incompleteMessage.Length - messageCompletedBytes;
+                        string message = System.Text.Encoding.UTF8.GetString(incompleteMessage);
+                        if (Message != null)
+                            Message(this, new MessageEventArgs(message));
+                        incompleteMessage = null;
+                    }
+                    else
+                    {
+                        Buffer.BlockCopy(receiveBuffer, offset, incompleteMessage, messageCompletedBytes,
+                            availableBytes);
+                        messageCompletedBytes += availableBytes;
+                        offset += availableBytes;
+                    }
+                }
+            }
+
+            if (client.Connected)
+                stream.BeginRead(receiveBuffer, 0, receiveBuffer.Length, HandleFinishedRead, null);
+            else
+                HandleNotConnected();
+        }
+
+        private byte[] receiveBuffer;
+        private byte[] incompleteMessage = null;
+        private byte[] incompleteMessageSize = null;
+        int messageCompletedBytes;
+        int messageSizeCompletedBytes;
+
+        private TcpClient client;
+        private NetworkStream stream;
+
+        int isOpened = 0;  // we use int to be able to use Interlocked.CompareExchange. value 0 means false, 1 - true
+
+        string host;
+        int port;
+    }
+}
