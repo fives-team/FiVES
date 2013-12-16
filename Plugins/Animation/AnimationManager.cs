01085a83f62ef496df2ee0e4fbff45c0e14dc864
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2013-12-16 09:25:35 +0100

    - Added AnimationManager and Animation classes - Adapted references

---
+++ Plugins/Animation/AnimationManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EventLoopPlugin;
+
+namespace AnimationPlugin
+{
+    internal class AnimationManager
+    {
+        public AnimationManager()
+        {
+            
+        }
+
+        private Dictionary<Guid, Animation> subscribedEntities = new Dictionary<Guid, Animation>();
+    }
+}
