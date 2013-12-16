4ff391abcc288e7e9e90ad97198328a5305fe885
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2013-12-16 09:32:03 +0100

    Animation Manager now subscribes to EventLoop

--- Plugins/Animation/AnimationManager.cs
+++ Plugins/Animation/AnimationManager.cs
@@ -10,7 +10,12 @@ namespace AnimationPlugin
     {
         public AnimationManager()
         {
-            
+            EventLoop.Instance.TickFired += new EventHandler<TickEventArgs>(handleEventTick);
+        }
+
+        private void handleEventTick(Object sender, TickEventArgs e)
+        {
+            Console.WriteLine("Handling EventTick for Animation at {0}", e.TimeStamp);
         }
 
         private Dictionary<Guid, Animation> subscribedEntities = new Dictionary<Guid, Animation>();
