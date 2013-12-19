dc96c1b19e2ae0babcf74794182b70059b579d06
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2013-12-19 08:58:45 +0100

    Implemented Animation class

--- Plugins/Animation/Animation.cs
+++ Plugins/Animation/Animation.cs
@@ -5,7 +5,27 @@ using System.Text;
 
 namespace AnimationPlugin
 {
-    class Animation
+    internal class Animation
     {
+        internal Animation(float startFrame, float endFrame)
+        {
+            StartFrame = startFrame;
+            CurrentFrame = startFrame;
+            EndFrame = endFrame;
+        }
+
+        internal float Tick(double frameDuration)
+        {
+            CurrentFrame += (float)frameDuration / 1000f;
+            if (CurrentFrame > EndFrame)
+            {
+                CurrentFrame = StartFrame + (CurrentFrame - EndFrame); // If CurrentFrame Exceeds endframe of current animation, resume it from start
+            }
+            return CurrentFrame;
+        }
+
+        private float StartFrame;
+        private float EndFrame;
+        private float CurrentFrame;
     }
 }
