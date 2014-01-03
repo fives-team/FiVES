4f32cff9762f247a761606adeb943f903ddea3a3
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-01-03 14:16:56 +0100

    Added Bot-Entities plugin project

---
+++ Plugins/BotEntities/BotEntitiesPluginInitializer.cs
@@ -0,0 +1,32 @@
+﻿using FIVES;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotEntitiesPlugin
+{
+    public class BotEntitiesPluginInitializer : IPluginInitializer
+    {
+        public string Name
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public List<string> PluginDependencies
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public List<string> ComponentDependencies
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
