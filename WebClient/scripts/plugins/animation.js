aa4248ae3a2e79b7aa7421a4849d9010bf4489a8
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-01-08 16:31:23 +0100

    Implemented client side animation class that allows entities to register themselves to animation updates

---
+++ WebClient/scripts/plugins/animation.js
@@ -0,0 +1,54 @@
+/**
+ * Created with JetBrains WebStorm.
+ * Author: Torsten Spieldenner
+ * Date: 1/8/14
+ * Time: 11:33 AM
+ * (c) DFKI 2014
+ * http://www.dfki.de
+ */
+
+var FIVES = FIVES || {};
+FIVES.Plugins = FIVES.Plugins || {};
+
+(function () {
+
+    "use strict";
+
+    var fps = 30;
+    var animation = function () {
+        window.setInterval(updateLoop, 1000.0 / fps);
+    };
+
+    var a = animation.prototype;
+
+    var registeredEntities = {};
+
+    function updateLoop() {
+        for (var entityGuid in registeredEntities) {
+            var entity = FIVES.Models.EntityRegistry._entities[entityGuid];
+            for(var animation in registeredEntities[entityGuid])
+            {
+                entity.increaseAnimationKey(registeredEntities[entityGuid][animation], fps);
+            }
+        }
+    }
+
+    a.registerToAnimationUpdates = function(entityGuid, animationName)
+    {
+        registeredEntities[entityGuid] = registeredEntities[entityGuid] || [];
+        var registeredEntityAnimations = registeredEntities[entityGuid];
+        if(registeredEntityAnimations.indexOf(animationName) < 0 )
+            registeredEntityAnimations.push(animationName);
+    };
+
+    a.unregisterFromAnimationUpdates = function(entityGuid, animationName) {
+        registeredEntities[entityGuid] = registeredEntities[entityGuid] || [];
+        var registeredEntityAnimations = registeredEntities[entityGuid];
+        var indexInRunningAnimations = registeredEntityAnimations.indexOf(animationName);
+        if(indexInRunningAnimations != -1)
+            registeredEntityAnimations.splice(indexInRunningAnimations, 1);
+    };
+
+
+    FIVES.Plugins.Animation = new animation();
+}());
