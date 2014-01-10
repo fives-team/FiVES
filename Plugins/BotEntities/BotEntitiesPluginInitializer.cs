d23ac5544b8ca3eff80589b169efd82af6a5429c
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-01-10 09:13:39 +0100

    BotEntities plugin now uses keyframe animation plugin for animation triggering of bots

--- Plugins/BotEntities/BotEntitiesPluginInitializer.cs
+++ Plugins/BotEntities/BotEntitiesPluginInitializer.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
 using EventLoopPlugin;
+using KeyframeAnimationPlugin;
 
 namespace BotEntitiesPlugin
 {
@@ -84,23 +85,54 @@ namespace BotEntitiesPlugin
         private void ChangeMoveSpeed(Entity botEntity)
         {
             if ((float)botEntity["velocity"]["x"] == botWalkSpeed)
+            {
                 botEntity["velocity"]["x"] = 0.0f;
+                StopWalkAnimation(botEntity);
+            }
             else
+            {
                 botEntity["velocity"]["x"] = botWalkSpeed;
+                PlayWalkAnimation(botEntity.Guid.ToString());
+            }
         }
 
         private void ChangeRotateSpeed(Entity botEntity)
         {
             int rotateRoll = random.Next(10);
             if (rotateRoll == 8)
+            {
                 botEntity["rotVelocity"]["r"] = botRotateSpeed;
+                PlayWalkAnimation(botEntity.Guid.ToString());
+            }
+
             else if (rotateRoll == 9)
+            {
                 botEntity["rotVelocity"]["r"] = -botRotateSpeed;
+                PlayWalkAnimation(botEntity.Guid.ToString());
+            }
             else
+            {
                 botEntity["rotVelocity"]["r"] = 0f;
+                StopWalkAnimation(botEntity);
+            }
         }
 
-        private int numBots = 10;
+        private void PlayWalkAnimation(string botGuid)
+        {
+            if (!KeyframeAnimationManager.Instance.IsPlaying(botGuid, "walk"))
+            {
+                KeyframeAnimation walkAnimation = new KeyframeAnimation("walk", 0f, 7.1f, -1, 1);
+                KeyframeAnimationManager.Instance.StartAnimation(botGuid, walkAnimation);
+            }
+        }
+
+        private void StopWalkAnimation(Entity botEntity)
+        {
+            if ((float)botEntity["velocity"]["x"] == 0f && (float)botEntity["rotVelocity"]["r"] == 0f)
+                KeyframeAnimationManager.Instance.StopAnimation(botEntity.Guid.ToString(), "walk");
+        }
+
+        private int numBots = 40;
         private HashSet<Entity> bots = new HashSet<Entity>();
         private string botMesh = "/models/natalieFives/xml3d/natalie.xml";
         private float botWalkSpeed = 0.05f;
