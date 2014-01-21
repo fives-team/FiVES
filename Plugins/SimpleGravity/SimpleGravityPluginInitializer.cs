8a52fd8108f4d55325ff77ffafcc35925be18588
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-01-21 13:37:31 +0100

    Added simple Gravity plugin project
    Conflicts:
    	FIVES.sln

---
+++ Plugins/SimpleGravity/SimpleGravityPluginInitializer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FIVES;
+
+namespace SimpleGravityPlugin
+{
+    public class SimpleGravityPluginInitializer : IPluginInitializer
+    {
+        public string Name
+        {
+            get { return "SimpleGravity"; }
+        }
+
+        public List<string> PluginDependencies
+        {
+            get { return new List<string>(); }
+        }
+
+        public List<string> ComponentDependencies
+        {
+            get { return new List<string> {"position"}; }
+        }
+
+        public void Initialize()
+        {
+            RegisterComponents();
+            RegisterToEvents();
+        }
+
+        public void Shutdown()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RegisterComponents() { }
+
+        private void RegisterToEvents() { }
+    }
+}
