9aa995854977ec17fa21b220fb8e5bf906c4cb8c
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-02-03 14:58:10 +0100

    Added comments, better line breaks

--- Plugins/Motion/MotionPluginInitializerTest.cs
+++ Plugins/Motion/MotionPluginInitializerTest.cs
@@ -19,6 +19,10 @@ namespace MotionPlugin
             RegisterLocationComponents();
         }
 
+        /// <summary>
+        /// Plugin and component dependencies are not resolved when using plugins in Unit tests.
+        /// Therefore, we need to register the components of Location plugin manually
+        /// </summary>
         private void RegisterLocationComponents()
         {
             var positionComponent = new ComponentDefinition("position");
@@ -54,6 +58,11 @@ namespace MotionPlugin
             Assert.IsNotNull(ComponentRegistry.Instance.FindComponentDefinition("rotVelocity"));
         }
 
+        /// <summary>
+        /// Tests if the velocity values are applied correctly to an entity's position.
+        /// Update computation is invoked manually once (usually done after TickFired
+        /// event of eventloop). Expected outcome is position increased by velocity
+        /// </summary>
         [Test()]
         public void ShouldApplyVelocityToPosition()
         {
@@ -66,11 +75,22 @@ namespace MotionPlugin
             Assert.AreEqual((float)e["position"]["z"], 0f);
         }
 
+        /// <summary>
+        /// Tests if rotational velocity is applied correctly to an entities orientation.
+        /// Update function is called manually once (usually done after TickFired event
+        /// of EventLoop).
+        /// Attention: Frequent conversion from Axis-Angle to Quaternion with Single
+        /// floating point precision introduces floating point inaccuracies, so we
+        /// do not test for equality of expected outcome, but if expected outcome is in
+        /// acceptable range.
+        /// </summary>
         [Test()]
         public void ShouldApplyRotVelocityToOrientation()
         {
             var e = new Entity();
-            Quat orientation = FIVES.Math.QuaternionFromAxisAngle(new Vector { x = 1, y = 0, z = 0 }, 0.1f);
+            Quat orientation = FIVES.Math.QuaternionFromAxisAngle(
+                new Vector { x = 1, y = 0, z = 0 }, 0.1f);
+
             e["orientation"]["x"] = orientation.x;
             e["orientation"]["y"] = orientation.y;
             e["orientation"]["z"] = orientation.z;
@@ -82,7 +102,8 @@ namespace MotionPlugin
             e["rotVelocity"]["r"] = 0.1f;
 
             plugin.UpdateSpin(e);
-            Quat entityOrientation = new Quat{ x = (float) e["orientation"]["x"],
+            Quat entityOrientation =
+                new Quat{ x = (float) e["orientation"]["x"],
                                                y = (float) e["orientation"]["y"],
                                                z = (float) e["orientation"]["z"],
                                                w = (float) e["orientation"]["w"]};
