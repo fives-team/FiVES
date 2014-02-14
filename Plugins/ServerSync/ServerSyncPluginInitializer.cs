4f16e16d84d3cfe4fd004ec1e6722d81b60f7d4d
Sergiy Byelozyorov <byelozyorov@cs.uni-saarland.de>
2014-02-14 14:56:43 +0100

    Created ServerSync plug-in

---
+++ Plugins/ServerSync/ServerSyncPluginInitializer.cs
@@ -0,0 +1,45 @@
+﻿using FIVES;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServerSyncPlugin
+{
+    public class ServerSyncPluginInitializer : IPluginInitializer
+    {
+        public string Name
+        {
+            get
+            {
+                return "ServerSync";
+            }
+        }
+
+        public List<string> PluginDependencies
+        {
+            get
+            {
+                return new List<string> { "KIARA" };
+            }
+        }
+
+        public List<string> ComponentDependencies
+        {
+            get
+            {
+                return new List<string>();
+            }
+        }
+
+        public void Initialize()
+        {
+
+        }
+
+        public void Shutdown()
+        {
+
+        }
+    }
+}
