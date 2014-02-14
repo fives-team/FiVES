01aa5604353e5d17c3f3f33ca3ee4e7525720398
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-02-14 12:18:56 +0100

    Created script file for avatar plugin

---
+++ WebClient/scripts/plugins/avatar/avatar.js
@@ -0,0 +1,17 @@
+/**
+ * Created with JetBrains WebStorm.
+ * Author: Torsten Spieldenner
+ * Date: 2/14/14
+ * Time: 12:18 PM
+ * (c) DFKI 2013
+ * http://www.dfki.de
+ */
+
+var FIVES = FIVES || {};
+FIVES.Plugins = FIVES.Plugins || {};
+
+(function () {
+
+    "use strict";
+
+}());
