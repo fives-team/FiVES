dfa98204662177e92bf441d97905f878235dbd12
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-02-14 13:46:01 +0100

    Added ClientSync plugin script to client

---
+++ WebClient/scripts/plugins/clientsync/clientsync.js
@@ -0,0 +1,16 @@
+/**
+ * Created with JetBrains WebStorm.
+ * Author: Torsten Spieldenner
+ * Date: 2/14/14
+ * Time: 1:44 PM
+ * (c) DFKI 2013
+ * http://www.dfki.de
+ */
+
+var FIVES = FIVES || {};
+FIVES.Plugins = FIVES.Plugins || {};
+
+(function(){
+    "use script";
+
+}());
