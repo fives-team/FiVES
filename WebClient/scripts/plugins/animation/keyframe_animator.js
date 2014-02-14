aaa7525d0b218958e4cca4e5119fa0c2eec2bbeb
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-02-14 11:18:31 +0100

    Added keyframe animator class to update entity animations

---
+++ WebClient/scripts/plugins/animation/keyframe_animator.js
@@ -0,0 +1,16 @@
+/**
+ * Created with JetBrains WebStorm.
+ * Author: Torsten Spieldenner
+ * Date: 2/14/14
+ * Time: 11:11 AM
+ * (c) DFKI 2013
+ * http://www.dfki.de
+ */
+
+var FIVES = FIVES || {};
+FIVES.Plugins = FIVES.Plugins || {};
+
+(function() {
+    "using strict";
+
+}());
