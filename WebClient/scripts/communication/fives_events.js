17b9c48f85b98ca7e77dd70912b32c3291a8221e
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-02-14 11:09:17 +0100

    Added OnComponentUpdated event handling

--- WebClient/scripts/communication/fives_events.js
+++ WebClient/scripts/communication/fives_events.js
@@ -11,6 +11,31 @@ var FIVES = FIVES || {};
 FIVES.Events = FIVES.Events || {};
 
 (function () {
-     "using strict";
+    "using strict";
 
+    /**
+     * List of functions that are executed when an entity fires an attribute changed event
+     * @type {Array}
+     */
+    var _onComponentUpdatedHandler = [];
+
+    FIVES.Events.AddOnComponentUpdatedHandler = function(handler) {
+        _onComponentUpdatedHandler.push(handler);
+    };
+
+    FIVES.Events.RemoveOnComponentUpdatedHandler = function(handler) {
+        if(_onComponentUpdatedHandler.indexOf(handler) != -1)
+        _onComponentUpdatedHandler.splice(handler, 1);
+    };
+
+    /**
+     * Fired when an entity updated one of its components. May be used by plugin scripts to executed
+     * @param entity [FIVES.Models.Entity] Entity object that fired the event
+     * @param componentName Name of the component that was updated
+     */
+    FIVES.Events.ComponentUpdated = function(entity, componentName) {
+        for(var i in _onComponentUpdatedHandler) {
+            _onComponentUpdatedHandler[i](entity, componentName);
+        }
+    }
 }());
