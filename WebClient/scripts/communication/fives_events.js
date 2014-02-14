165ade794db7c190ce28bda68e90085c22f142fc
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-02-14 11:00:44 +0100

    Added event manager object to FiVES client

---
+++ WebClient/scripts/communication/fives_events.js
@@ -0,0 +1,16 @@
+/**
+ * Created with JetBrains WebStorm.
+ * Author: Torsten Spieldenner
+ * Date: 2/14/14
+ * Time: 10:59 AM
+ * (c) DFKI 2013
+ * http://www.dfki.de
+ */
+
+var FIVES = FIVES || {};
+FIVES.Events = FIVES.Events || {};
+
+(function () {
+     "using strict";
+
+}());
