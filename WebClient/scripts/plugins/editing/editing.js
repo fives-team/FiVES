ac60bc651f168fffd8693272e6d510dcca2f69d5
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-02-17 12:15:39 +0100

    Added scriptfile for editing plugin

---
+++ WebClient/scripts/plugins/editing/editing.js
@@ -0,0 +1,22 @@
+/**
+ * Created with JetBrains WebStorm.
+ * Author: Torsten Spieldenner
+ * Date: 2/17/14
+ * Time: 12:14 PM
+ * (c) DFKI 2013
+ * http://www.dfki.de
+ */
+
+var FIVES = FIVES || {};
+FIVES.Plugins = FIVES.Plugins || {};
+
+(function () {
+    "use script";
+
+    var editing = function() {};
+
+    var e = editing.prototype;
+
+    FIVES.Plugins.Editing = new editing();
+}());
+
