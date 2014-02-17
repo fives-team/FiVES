6f1aff94d4f856edb36b9dd73fbb4fbf4597187a
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-02-17 12:30:03 +0100

    Created scripting plugin script for client

--- WebClient/scripts/communication/fives_communicator.js
+++ WebClient/scripts/communication/fives_communicator.js
@@ -118,8 +118,6 @@ FIVES.Communication = FIVES.Communication || {};
             this.registeredWrapperRegisterers[i]();
         }
 
-        this.createServerScriptFor = this.connection.generateFuncWrapper("scripting.createServerScriptFor");
-
         this.updateMotion = this.connection.generateFuncWrapper("motion.update");
     };
 
