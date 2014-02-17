9ad3d1bbbc4c5bf894c6e7d0e994be3fcefa2ff2
Sergiy Byelozyorov <byelozyorov@cs.uni-saarland.de>
2014-02-17 11:57:55 +0100

    Implemented ignoring updates which were causes by sync

--- Plugins/ServerSync/WorldSync.cs
+++ Plugins/ServerSync/WorldSync.cs
@@ -1,4 +1,5 @@
 using FIVES;
+using KIARAPlugin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -43,7 +44,7 @@ namespace ServerSyncPlugin
             }
         }
 
-        private static void SyncFullEntityToServer(IRemoteServer server, Entity entity)
+        void SyncFullEntityToServer(IRemoteServer server, Entity entity)
         {
             server.Connection["serverSync.addEntity"](entity.Guid);
             foreach (var component in entity.Components)
@@ -67,27 +68,61 @@ namespace ServerSyncPlugin
 
         void RegisterWorldSyncAPI()
         {
-            ServerSync.LocalServer.Service["serverSync.addEntity"] = (Action<Guid>)AddEntity;
-            ServerSync.LocalServer.Service["serverSync.removeEntity"] = (Action<Guid>)RemoveEntity;
+            ServerSync.LocalServer.Service["serverSync.addEntity"] = (Action<Connection, Guid>)AddEntity;
+            ServerSync.LocalServer.Service["serverSync.removeEntity"] = (Action<Connection, Guid>)RemoveEntity;
             ServerSync.LocalServer.Service["serverSync.changeAttribute"] =
-                (Action<Guid, string, string, object>)ChangeAttribute;
+                (Action<Connection, Guid, string, string, object>)ChangeAttribute;
         }
 
-        void AddEntity(Guid entityGuid)
+        void AddEntity(Connection connection, Guid entityGuid)
         {
-            // TODO
+            if (World.Instance.ContainsEntity(entityGuid))
+                throw new Exception("Requested addition of an existing entity");
+
+            // TODO: Forward update to other servers.
+
+            // TODO: Create entity sync info.
+
+            ignoredEntityAdditions.Add(entityGuid);
+            World.Instance.Add(new Entity(entityGuid));
+
             throw new NotImplementedException();
         }
 
-        void RemoveEntity(Guid entityGuid)
+        void RemoveEntity(Connection connection, Guid entityGuid)
         {
-            // TODO
+            if (!World.Instance.ContainsEntity(entityGuid))
+                throw new Exception("Requested removal of non-existing entity");
+
+            // TODO: Forward update to other servers.
+
+            // TODO: Mark entity as removed in sync info.
+
+            ignoredEntityRemovals.Add(entityGuid);
+            World.Instance.Remove(World.Instance.FindEntity(entityGuid));
+
             throw new NotImplementedException();
         }
 
-        void ChangeAttribute(Guid entityGuid, string componentName, string attributeName, object newValue)
+        void ChangeAttribute(Connection connection, Guid entityGuid, string componentName, string attributeName,
+            object newValue)
         {
-            // TODO
+            if (!World.Instance.ContainsEntity(entityGuid))
+                throw new Exception("Requested attribute change for non-existing entity");
+
+            // TODO: Forward update to other servers.
+
+            // TODO: Update sync info.
+
+            AttributeUpdate update = new AttributeUpdate
+            {
+                EntityGuid = entityGuid,
+                ComponentName = componentName,
+                AttributeName = attributeName,
+                NewValue = newValue
+            };
+            ignoredAttributeUpdates.Add(update);
+
             throw new NotImplementedException();
         }
 
@@ -99,6 +134,12 @@ namespace ServerSyncPlugin
 
         void HandleAddedEntityToWorld(object sender, EntityEventArgs e)
         {
+            if (ignoredEntityAdditions.Contains(e.Entity.Guid))
+            {
+                ignoredEntityAdditions.Remove(e.Entity.Guid);
+                return;
+            }
+
             foreach (var server in ServerSync.RemoteServers)
                 if (server.DoI.IsInterestedInEntity(e))
                     server.Connection["serverSync.addEntity"](e.Entity.Guid);
@@ -108,6 +149,12 @@ namespace ServerSyncPlugin
 
         void HandleRemovedEntityFromWorld(object sender, EntityEventArgs e)
         {
+            if (ignoredEntityRemovals.Contains(e.Entity.Guid))
+            {
+                ignoredEntityRemovals.Remove(e.Entity.Guid);
+                return;
+            }
+
             foreach (var server in ServerSync.RemoteServers)
                 if (server.DoI.IsInterestedInEntity(e))
                     server.Connection["serverSync.removeEntity"](e.Entity.Guid);
@@ -115,6 +162,23 @@ namespace ServerSyncPlugin
 
         private void HandleEntityAttributeChanged(object sender, ChangedAttributeEventArgs e)
         {
+            var update = new AttributeUpdate
+            {
+                EntityGuid = e.Entity.Guid,
+                ComponentName = e.Component.Name,
+                AttributeName = e.AttributeName,
+                NewValue = e.NewValue
+            };
+
+            foreach (var ignoredUpdate in ignoredAttributeUpdates)
+            {
+                if (ignoredUpdate.Equals(update))
+                {
+                    ignoredAttributeUpdates.Remove(ignoredUpdate);
+                    return;
+                }
+            }
+
             foreach (var server in ServerSync.RemoteServers)
             {
                 if (server.DoI.IsInterestedInAttributeChange(e))
@@ -124,5 +188,31 @@ namespace ServerSyncPlugin
                 }
             }
         }
+
+        private class AttributeUpdate
+        {
+            public Guid EntityGuid;
+            public string ComponentName;
+            public string AttributeName;
+            public object NewValue;
+
+            public override bool Equals(object otherObject)
+            {
+                if (otherObject is AttributeUpdate)
+                {
+                    var otherUpdate = otherObject as AttributeUpdate;
+                    return EntityGuid.Equals(otherUpdate.EntityGuid)
+                        && ComponentName.Equals(otherUpdate.ComponentName)
+                        && AttributeName.Equals(otherUpdate.AttributeName)
+                        && NewValue.Equals(otherUpdate.NewValue);
+                }
+
+                return false;
+            }
+        }
+
+        private HashSet<Guid> ignoredEntityAdditions = new HashSet<Guid>();
+        private HashSet<Guid> ignoredEntityRemovals = new HashSet<Guid>();
+        private List<AttributeUpdate> ignoredAttributeUpdates = new List<AttributeUpdate>();
     }
 }
