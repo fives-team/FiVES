9ea8466926bd4a4a3e0a2c69231b220768fb0fcd
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-02-19 09:26:09 +0100

    Added Avatar_Collision Client script

---
+++ WebClient/scripts/plugins/avatar_collision/avatar_collision.js
@@ -0,0 +1,23 @@
+/**
+ * Created with JetBrains WebStorm.
+ * Author: Torsten Spieldenner
+ * Date: 2/19/14
+ * Time: 9:23 AM
+ * (c) DFKI 2013
+ * http://www.dfki.de
+ */
+
+var FIVES = FIVES || {};
+FIVES.Plugins = FIVES.Plugins || {};
+
+(function () {
+    "use strict";
+
+    var avatarCollision = function () {
+
+    };
+
+    var a = avatarCollision.prototype;
+
+}());
+
