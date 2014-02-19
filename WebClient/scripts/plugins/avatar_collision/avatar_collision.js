391bc9339ae8f32701047fe364f760cb3543514e
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-02-19 09:32:20 +0100

    Implemented function to get hitpoint with avatar mesh on ground in client

--- WebClient/scripts/plugins/avatar_collision/avatar_collision.js
+++ WebClient/scripts/plugins/avatar_collision/avatar_collision.js
@@ -13,11 +13,30 @@ FIVES.Plugins = FIVES.Plugins || {};
 (function () {
     "use strict";
 
-    var avatarCollision = function () {
+    var _xml3dElement;
 
+    var avatarCollision = function () {
+        _xml3dElement = $("xml3d")[0];
     };
 
     var a = avatarCollision.prototype;
+
+    a.getHitpointWithGround = function(entity) {
+        var rayOrigin = getCollisionRayOrigin(entity);
+        var ray = new XML3DRay(rayOrigin, new XML3DVec3(0,-1,0));
+        var outHitpoint = new XML3DVec3(0,0,0);
+        _xml3dElement.getElementByRay(ray, outHitpoint);
+        return outHitpoint;
+    };
+
+    var getCollisionRayOrigin = function(entity) {
+        var boundingBox = entity.xml3dView.groupElement.getBoundingBox();
+        var center = boundingBox.center();
+        var view = $(_xml3dElement.activeView)[0];
+        var viewDirection = view.getDirection();
+        var rayOrigin = new XML3DVec3(center.x + viewDirection.x * 0.5, boundingBox.max.y, center.z + viewDirection.z * 0.5);
+        return rayOrigin;
+    };
 
 }());
 
