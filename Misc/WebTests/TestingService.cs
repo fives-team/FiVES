ffd05fed40f49bed7abbff19d684f3c47c89788e
Sergiy Byelozyorov <byelozyorov@cs.uni-saarland.de>
2014-02-20 22:10:26 +0100

    Adapted WebTests to implement IPC service and wait for server started event

---
+++ Misc/WebTests/TestingService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using TestingPlugin;
+
+namespace WebTests
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    class TestingService : ITestingService
+    {
+        public event EventHandler ServerStarted;
+
+        public void NotifyServerStarted()
+        {
+            if (ServerStarted != null)
+                ServerStarted(this, new EventArgs());
+        }
+    }
+}
