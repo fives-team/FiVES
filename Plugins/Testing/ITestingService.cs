10ddbb3a4fa2b296dbbab70841bf78694a1ce48e
Sergiy Byelozyorov <byelozyorov@cs.uni-saarland.de>
2014-02-20 22:08:02 +0100

    Created Testing plug-in

---
+++ Plugins/Testing/ITestingService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace TestingPlugin
+{
+    [ServiceContract(Namespace = "http://sergiyb.com")]
+    public interface ITestingService
+    {
+        [OperationContract]
+        void NotifyServerStarted();
+    }
+}
