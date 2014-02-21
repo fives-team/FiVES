8957e04f22e3b4f462c4cda74b2640762bba0b29
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-02-21 10:04:42 +0100

    Line breaks

--- Plugins/AvatarCollision/AvatarCollisionPluginInitializer.cs
+++ Plugins/AvatarCollision/AvatarCollisionPluginInitializer.cs
@@ -9,12 +9,12 @@ using FIVES;
 namespace AvatarCollisionPlugin
 {
     /// <summary>
-    /// AvatarCollision introduces an attribute that specifies the current ground level for an avatar as determined by a client.
-    /// If the groundlevel is set (may not be the case for non-avatar entities), the entitiy is automatically put to this height.
-    /// Collision with geometry in moving direction of the avatar is handled by the client setting velocity to 0.
-    /// Having the height of the avatar set on the server avoids sending incorrect collision values to the client which then have
-    /// to be resent to the server in corrected form. Instead, the server checks for correct position and sends a correct vector
-    /// immediately.
+    /// AvatarCollision introduces an attribute that specifies the current ground level for an avatar as determined
+    /// by a client. If the groundlevel is set (may not be the case for non-avatar entities), the entitiy is
+    /// automatically put to this height. Collision with geometry in moving direction of the avatar is handled by
+    /// the client setting velocity to 0. Having the height of the avatar set on the server avoids sending incorrect
+    /// collision values to the client which then have to be resent to the server in corrected form. Instead, the
+    /// server checks for correct position and sends a correct vector immediately.
     /// </summary>
     public class AvatarCollisionPluginInitializer : IPluginInitializer
     {
@@ -71,20 +71,22 @@ namespace AvatarCollisionPlugin
         }
 
         /// <summary>
-        /// Sets initial grouns level of an entity to its y-attribute. This is as when just added the entity the client
-        /// may not yet have determined the ground level (as no ray was cast). To avoid setting the entity to a wrong level,
-        /// it is just kept where it is until the first ray is cast
+        /// Sets initial ground level of an entity to its y-attribute. This is as when just added the entity the
+        /// client may not yet have determined the ground level (as no ray was cast). To avoid setting the entity
+        /// to a wrong level, it is just kept where it is until the first ray is cast
         /// </summary>
         /// <param name="sender">The World</param>
         /// <param name="e">Entity Added event arguments</param>
         private void HandleEntityAdded(Object sender, EntityEventArgs e)
         {
-            e.Entity["avatarCollision"]["groundLevel"] = ((Vector)e.Entity["location"]["position"]).y; // Initialise entities without gravity
+            e.Entity["avatarCollision"]["groundLevel"] =
+                ((Vector)e.Entity["location"]["position"]).y; // Initialise entities without gravity
             e.Entity.ChangedAttribute += new EventHandler<ChangedAttributeEventArgs>(HandleAttributeChanged);
         }
 
         /// <summary>
-        /// Checks if position of entity has changed and sets y attribute to ground level if y and ground level are different
+        /// Checks if position of entity has changed and sets y attribute to ground level if y and ground level
+        /// are different
         /// </summary>
         /// <param name="sender">Entity that changed position</param>
         /// <param name="e">The Attribute changed Event Args</param>
