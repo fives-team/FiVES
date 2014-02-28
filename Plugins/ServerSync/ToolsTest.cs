33b091582793e2ba257df0785307712dcea63ca3
Sergiy Byelozyorov <byelozyorov@cs.uni-saarland.de>
2014-02-28 21:04:08 +0100

    Added unit tests for Tools and StringSerialization classes

---
+++ Plugins/ServerSync/ToolsTest.cs
@@ -0,0 +1,24 @@
+﻿using KIARAPlugin;
+using Moq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace ServerSyncPlugin
+{
+    [TestFixture]
+    public class ToolsTest
+    {
+        [Test]
+        public void ShouldSetTypeNameHandlingToAutoWhenConfiguringJsonSerializer()
+        {
+            Mock<Connection> conn = new Mock<Connection>();
+            object settings = new JsonSerializerSettings();
+            conn.Setup(c => c.GetProperty("JsonSerializerSettings", out settings)).Returns(true);
+
+            Tools.ConfigureJsonSerializer(conn.Object);
+
+            conn.Verify(c => c.SetProperty("JsonSerializerSettings",
+                It.Is<JsonSerializerSettings>(jss => jss.TypeNameHandling == TypeNameHandling.Auto)));
+        }
+    }
+}
