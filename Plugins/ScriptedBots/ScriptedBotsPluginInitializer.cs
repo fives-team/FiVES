1ab2be01008487ddc4fef1003d602825424b1e77
Sergiy Byelozyorov <byelozyorov@cs.uni-saarland.de>
2014-03-24 18:48:47 +0100

    Created a ScriptedBot plugin

---
+++ Plugins/ScriptedBots/ScriptedBotsPluginInitializer.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FIVES;
+using System.IO;
+
+namespace ScriptedBotsPlugin
+{
+    public class ScriptedBotsPluginInitializer : IPluginInitializer
+    {
+        public string Name
+        {
+            get
+            {
+                return "ScriptedBots";
+            }
+        }
+
+        public List<string> PluginDependencies
+        {
+            get
+            {
+                return new List<string> { "Scripting" };
+            }
+        }
+
+        public List<string> ComponentDependencies
+        {
+            get
+            {
+                return new List<string> { "location", "mesh", "scripting" };
+            }
+        }
+
+        public void Initialize()
+        {
+            LoadConfig();
+            LoadScript();
+
+            for (int i = 0; i < numBotsToCreate; i++)
+                CreateScriptedBot();
+        }
+
+        private void LoadScript()
+        {
+            // TODO: implement all global variables used in the script
+            botScript = File.ReadAllText(GetResourceFile("botScript.js"));
+        }
+
+        private void CreateScriptedBot()
+        {
+            var botEntity = new Entity();
+            botEntity["mesh"]["uri"] = "";
+            botEntity["scripting"]["serverScript"] = botScript;
+            botEntity["scripting"]["serverScriptDeps"] = "eventLoop.addTickFiredHandler != undefined && " +
+                "eventLoop.intervalMs != undefined && entity['location'] != null && entity['motion'] != null";
+            botEntity["location"]["position"] =
+                new Vector((float)(random.NextDouble() * 13 + 1), 0f, (float)(random.NextDouble() * 13 + 1));
+            World.Instance.Add(botEntity);
+        }
+
+        /// <summary>
+        /// Converts a file name to the URI that point to the file as if it was located in the same directory as the
+        /// current assembly.
+        /// </summary>
+        /// <param name="configFilename"></param>
+        /// <returns></returns>
+        private string GetResourceFile(string configFilename)
+        {
+            return Path.Combine(Path.GetDirectoryName(this.GetType().Assembly.Location), configFilename);
+        }
+
+        private void LoadConfig()
+        {
+            // TODO: replace with configuration file when a per-plugin configuration system is implemented
+            numBotsToCreate = 1;
+        }
+
+        public void Shutdown()
+        {
+        }
+
+        int numBotsToCreate;
+        string botScript;
+        Random random = new Random();
+    }
+}
---
+++ Plugins/ScriptedBots/botScript.js
@@ -0,0 +1,44 @@
+﻿function vectorToString(vector) {
+    return vector.x + " " + vector.y + " " + vector.z;
+}
+
+function isInsideBlock(pos) {
+    return (pos.x > 0 ? pos.x % 75 > 15 : pos.x % 75 > -60) &&
+        (pos.z > 0 ? pos.z % 75 > 15 : pos.z % 75 > -60);
+}
+
+function computeNextPosition() {
+    var pos = entity["location"]["position"];
+    var vel = entity["motion"]["velocity"];
+    return new Vector(eventLoop.intervalMs * vel.x + pos.x,
+                      eventLoop.intervalMs * vel.y + pos.y,
+                      eventLoop.intervalMs * vel.z + pos.z);
+}
+
+function changeDirection() {
+    var angle = Math.random() * 2 * Math.PI;
+    var speed = 3 * eventLoop.intervalMs / 1000;
+    entity["motion"]["velocity"] = new Vector(Math.sin(angle) * speed, 0, Math.cos(angle) * speed);
+    //logger.debug('bot velocity: ' + vectorToString(entity['motion']['velocity']));
+}
+
+eventLoop.addTickFiredHandler(function (timespan) {
+    // keep changing direction until the next position is not going to be inside of a block
+    while (isInsideBlock(computeNextPosition()))
+        changeDirection();
+
+    // also change direction randomly on average every 10 seconds
+    var numCallsPerSecond = 1000 / eventLoop.intervalMs;
+    var prob = 1 / (numCallsPerSecond * 10);
+    if (Math.random() < prob) {
+        changeDirection();
+    }
+
+    //logger.debug('bot position: ' + vectorToString(entity['location']['position']));
+});
+
+// start movement
+changeDirection();
+
+//logger.debug('bot position: ' + vectorToString(entity['location']['position']));
+//logger.debug('bot velocity: ' + vectorToString(entity['motion']['velocity']));
