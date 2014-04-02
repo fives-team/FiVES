a9eb48c86036ce957c2cf93b18c27854d80d68f4
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-02 14:55:22 +0200

    Added comments

--- Core/DomainModel/ServiceBus/AccumulatedAttributeTransform.cs
+++ Core/DomainModel/ServiceBus/AccumulatedAttributeTransform.cs
@@ -6,14 +6,28 @@ using FIVES;
 
 namespace FIVESServiceBus
 {
+    /// <summary>
+    /// Stores all Attribute Transformations that are performed during a service chain computation in the service bus.
+    /// It maintains a list of all recent intermediate results. Provides a function to query the latest actual
+    /// attribute value of a transformed entity.
+    /// </summary>
     public class AccumulatedAttributeTransform : EventArgs
     {
-        public AccumulatedAttributeTransform(Entity entity, Dictionary<string, Dictionary<string, object>> accumulatedTransforms)
+        public AccumulatedAttributeTransform(Entity entity,
+            Dictionary<string, Dictionary<string, object>> accumulatedTransforms)
         {
             this.accumulatedTransforms = accumulatedTransforms;
             this.entity = entity;
         }
 
+        /// <summary>
+        /// Adds a transformation for an attribute to the accumulated transformation. If the same attribute was changed
+        /// before, its value is replaced by the new one. If it was not in the list before, it is added to the list
+        /// of transformations
+        /// </summary>
+        /// <param name="componentName">Component of which attribute should be changed</param>
+        /// <param name="attributeName">Changed Attribute</param>
+        /// <param name="newValue">New value of the attribute</param>
         public void AddAttributeTransformation(string componentName, string attributeName, object newValue)
         {
             if (!accumulatedTransforms.ContainsKey(componentName))
@@ -24,6 +38,14 @@ namespace FIVESServiceBus
                 accumulatedTransforms[componentName][attributeName] = newValue;
         }
 
+        /// <summary>
+        /// Retrieves the current value of an attribute of the entity that is undergoing transformation. If it
+        /// was transformed before, and thus is present in the accumulated transforms, this recent value is
+        /// returned. Otherwise, the value contained in the ECA
+        /// </summary>
+        /// <param name="componentName">Component that contains the requested attribute</param>
+        /// <param name="attributeName">Name of the requested attribute</param>
+        /// <returns>Current attribute value</returns>
         public object CurrentAttributeValue(string componentName, string attributeName)
         {
             if (accumulatedTransforms.ContainsKey(componentName)
@@ -37,6 +59,9 @@ namespace FIVESServiceBus
             }
         }
 
+        /// <summary>
+        /// Registry of all attribute transformations that were performed so far
+        /// </summary>
         public Dictionary<string, Dictionary<string, object>> AccumulatedTransformations
         {
             get
@@ -45,6 +70,9 @@ namespace FIVESServiceBus
             }
         }
 
+        /// <summary>
+        /// Entity that is undergoing transformation
+        /// </summary>
         public Entity Entity
         {
             get { return entity; }
