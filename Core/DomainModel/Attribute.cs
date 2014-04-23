02695d6fbcf3222d9d444d282f158e8acbb010fe
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-23 10:11:17 +0200

    Attribute now does not Invoke ServiceBus functions directly, but triggers proposed-Event when proposing an attribute value

--- Core/DomainModel/Attribute.cs
+++ Core/DomainModel/Attribute.cs
@@ -45,7 +45,7 @@ namespace FIVES
                                                         Definition.Name,
                                                         newValue);
 
-                ServiceBus.ServiceGateway.PublishTransformation(proposedChange);
+                ParentComponent.ContainingEntity.PublishAttributeChangeSuggestion(proposedChange);
             }
             else
             {
