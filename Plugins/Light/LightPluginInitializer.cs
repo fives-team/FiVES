8b578074edd859c0ed75d496780a9c7233cc7484
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-10-10 10:43:28 +0200

    Added plugin for lights

---
+++ Plugins/Light/LightPluginInitializer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FIVES;
+
+namespace Light
+{
+    public class LightPluginInitializer : IPluginInitializer
+    {
+        public string Name
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public List<string> PluginDependencies
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public List<string> ComponentDependencies
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Shutdown()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
