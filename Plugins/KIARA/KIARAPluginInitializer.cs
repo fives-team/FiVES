d28b928ddf315a5acf16672fd34c1a07efcf6dbb
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2015-02-06 10:11:46 +0100

    Added new KIARA Plugin

---
+++ Plugins/KIARA/KIARAPluginInitializer.cs
@@ -0,0 +1,37 @@
+﻿using FIVES;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KIARAPlugin
+{
+    public class KIARAPluginInitializer : IPluginInitializer
+    {
+        public string Name
+        {
+            get { return "KIARA"; }
+        }
+
+        public List<string> PluginDependencies
+        {
+            get { return new List<string>(); }
+        }
+
+        public List<string> ComponentDependencies
+        {
+            get { return new List<string>(); }
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Shutdown()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
