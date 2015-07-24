3e2f8ad982beb626ae3f1feba55c8975c195044f
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2015-07-24 16:37:44 +0200

    Nicer position of includes

--- Plugins/RESTService/RESTSceneAPI/DeleteResolver.cs
+++ Plugins/RESTService/RESTSceneAPI/DeleteResolver.cs
@@ -1,5 +1,4 @@
-﻿using FIVES;
-// This file is part of FiVES.
+﻿// This file is part of FiVES.
 //
 // FiVES is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -12,6 +11,7 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with FiVES.  If not, see <http://www.gnu.org/licenses/>.
+using FIVES;
 using System;
 using System.Collections.Generic;
 using System.Linq;
