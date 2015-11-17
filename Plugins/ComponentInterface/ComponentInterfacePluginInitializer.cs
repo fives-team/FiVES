4141db39f67b2473a1395bf99b74900d4f7cd9a9
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2015-11-17 14:13:19 +0100

    Added ComponentInterface Plugin project

---
+++ Plugins/ComponentInterface/ComponentInterfacePluginInitializer.cs
@@ -0,0 +1,37 @@
+﻿using FIVES;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComponentInterface
+{
+    public class ComponentInterfacePluginInitializer : IPluginInitializer
+    {
+        public string Name
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public List<string> PluginDependencies
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public List<string> ComponentDependencies
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Shutdown()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
