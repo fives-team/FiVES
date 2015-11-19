00e9295e322a2106153191409e5f13276ebd16fc
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2015-11-19 10:56:45 +0100

    Exposed "Register Component" functionality via SINFONI

--- Plugins/ComponentInterface/ComponentInterfacePluginInitializer.cs
+++ Plugins/ComponentInterface/ComponentInterfacePluginInitializer.cs
@@ -46,5 +46,42 @@ namespace ComponentInterfacePlugin
         public void Shutdown()
         {
         }
+
+        private void RegisterSinfoniService()
+        {
+            ClientManager.Instance.RegisterClientService("component", true, new Dictionary<string, Delegate> {
+                    { "registerComponent", (Action<ExternalComponentDefinition>)HandleExternalRegisterRequest }
+                });
+        }
+
+        private void HandleExternalRegisterRequest(ExternalComponentDefinition definition)
+        {
+            try
+            {
+                ComponentDefinition c = RegisterExternalComponent(definition);
+                registeredExternalComponents.Add(c);
+            }
+            catch(Exception e)
+            {
+                // SINFONI NEEDS A HANDLE TO PROCESS EXCEPTIONS TO THE CLIENT !!
+                // https://github.com/tospie/SINFONI/issues/3
+                // Until then, we log the error in the console
+                Console.WriteLine("Cannot Register Component: " + e.Message + "\n" + e.InnerException);
+            }
+        }
+
+        private ComponentDefinition RegisterExternalComponent(ExternalComponentDefinition definition)
+        {
+            ComponentDefinition c = new ComponentDefinition(definition.Name);
+            foreach (ExternalAttributeDefinition attribute in definition.Attributes.Values)
+            {
+                Type t = Type.GetType(attribute.Type);
+                c.AddAttribute(attribute.Name, t, attribute.defaultValue);
+            }
+            ComponentRegistry.Instance.Register(c);
+            return c;
+        }
+
+        HashSet<ComponentDefinition> registeredExternalComponents = new HashSet<ComponentDefinition>();
     }
 }
