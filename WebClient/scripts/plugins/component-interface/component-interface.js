b9a2c66a354a1856417c92f1ab27f63da9be3d60
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2015-11-25 15:20:07 +0100

    Some comments

--- WebClient/scripts/plugins/component-interface/component-interface.js
+++ WebClient/scripts/plugins/component-interface/component-interface.js
@@ -15,6 +15,21 @@
 var FIVES = FIVES || {};
 FIVES.Plugins = FIVES.Plugins || {};
 
+
+/**
+ * COMPONENT INTERFACE PLUGIN
+ *
+ * This client-side can be used to communicate with the server-side counterpart "Component Interface" . Component
+ * Interface exposes the API to register new components for clients to register new component types during run-time
+ *
+ * Usage:
+ * - Initialize a new component definition object (FIVES.Plugins.ComponentInterface.initializeComponent)
+ * - Add Attribute definitions using the respective method on the returned component definition object
+ * - call the remote procedure (FIVES.Plugins.ComponentInterface.RegisterComponent) with the component object as
+ *    parameter
+ *
+ */
+
 (function(){
     "use strict";
 
@@ -27,14 +42,29 @@ FIVES.Plugins = FIVES.Plugins || {};
     var c = component_interface.prototype;
 
     c._registerSinfoniFunctions = function() {
-
+        /**
+         * Remote Procedure Call to the server-side SINFONI service to register a new component. Uses a previously
+         * initialized component object with a list of added attributes as parameter
+         * @param definition ComponentDefinition object
+         */
         this.registerComponent = _communicator.connection.generateFuncWrapper("component.registerComponent");
     };
 
+    /**
+     * Initializes a new component object
+     * @param componentName Unique name of the new component
+     * @returns {{Name: *, Attributes: Array, addAttribute: Function}} Initialized component object
+     */
     c.initializeComponent = function(componentName) {
         return { Name: componentName, Attributes: [], addAttribute: addAttribute};
     };
 
+    /**
+     * Adds a new Attribute definition to a previously initialized component definition object
+     * @param attributeName Unique name of the attribute within the component
+     * @param sinfoniType Name of the Type in the SINFONI IDL for the new attribute
+     * @param defaultValue (optional) Default value of the new attribute
+     */
     var addAttribute = function(attributeName, sinfoniType, defaultValue)
     {
         var attr = {Name: attributeName, Type: sinfoniType};
