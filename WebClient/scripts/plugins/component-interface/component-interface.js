5a6fc8801bbae207bdb2347f4631ddfa9fc6b780
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2015-11-25 13:55:27 +0100

    Added component interface plugin file

---
+++ WebClient/scripts/plugins/component-interface/component-interface.js
@@ -0,0 +1,22 @@
+// This file is part of FiVES.
+//
+// FiVES is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License version 3
+// (LGPL v3) as published by the Free Software Foundation.
+//
+// FiVES is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU LGPL License
+// along with FiVES.  If not, see <http://www.gnu.org/licenses/>
+
+var FIVES = FIVES || {};
+FIVES.Plugins = FIVES.Plugins || {};
+
+(function(){
+    "use strict";
+
+}());
+
