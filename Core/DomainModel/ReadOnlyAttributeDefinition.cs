9310563ae5cf12dd9e75462a5fd9010c0363004b
Johannes Ebersold <johannes.ebersold@dfki.de>
2015-12-09 17:25:49 +0100

    The Attribute Defintion knows if its something that offers notification on collection or property changed

--- Core/DomainModel/ReadOnlyAttributeDefinition.cs
+++ Core/DomainModel/ReadOnlyAttributeDefinition.cs
@@ -29,6 +29,9 @@ namespace FIVES
             Guid = guid;
             Name = name;
             Type = type;
+			var interfaces = type.GetInterfaces();
+			HasNotifyCollectionChangedNotification = interfaces.Contains(typeof(System.Collections.Specialized.INotifyCollectionChanged));
+			HasPropertyChangedNotification = interfaces.Contains(typeof(System.ComponentModel.INotifyPropertyChanged));
 
             try
             {
@@ -69,6 +72,16 @@ namespace FIVES
         /// Type of the attribute.
         /// </summary>
         public Type Type { get; private set; }
+
+		/// <summary>
+		/// Attribute implements System.Collections.Specialized.INotifyCollectionChanged
+		/// </summary>
+		public bool HasNotifyCollectionChangedNotification {get; private set;}
+
+		/// <summary>
+		/// Attribute implements System.ComponentModel.INotifyPropertyChanged
+		/// </summary>
+		public bool HasPropertyChangedNotification {get; private set;}
 
         private static T Cast<T>(object o)
         {
