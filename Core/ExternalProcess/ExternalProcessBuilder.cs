fd4ec82693c2006035a2d91d91c5df507905ec46
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2016-06-23 15:30:15 +0200

    Applied patch that added external process builder

---
+++ Core/ExternalProcess/ExternalProcessBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FIVES
+{
+    public class ExternalProcessBuilder
+    {
+
+		private ExternalProcess externalProcess;
+		private string parameters;
+
+		public ExternalProcessBuilder() {
+			externalProcess = new ExternalProcess();
+			parameters = "";
+		}
+
+		public ExternalProcessBuilder ExecutableName(String name) {
+			externalProcess.ExecutableName = name;
+			return this;
+		}
+
+		public ExternalProcessBuilder AppendParameter(String parameter) {
+			parameters += " " + parameter;
+			return this;
+		}
+
+		public ExternalProcessBuilder WorkingDirectory(String workingDirectory) {
+			externalProcess.WorkingDirectory = workingDirectory;
+			return this;
+		}
+
+		public ExternalProcess Build() {
+			externalProcess.Parameters = parameters;
+			return externalProcess;
+		}
+
+    }
+}
