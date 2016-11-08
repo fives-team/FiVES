d413d930bc337ac39311a28532d63e6cbb6b8e80
Michael Guldner <migu01@dfki.de>
2016-11-08 15:21:07 +0100

    added diagnosis interface as a first draft

---
+++ Plugins/Diagnosis/DiagnosisInterface.cs
@@ -0,0 +1,32 @@
+﻿using FIVES;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiagnosisPlugin
+{
+    class DiagnosisInterface
+    {
+        public static DiagnosisInterface Instance = new DiagnosisInterface();
+
+        public void IntroducePlugin(IPluginInitializer pluginInitializer)
+        {
+            registeredPlugins.Add(pluginInitializer.Name);
+        }
+
+        public void IntroduceValue(string valueName, object value)
+        {
+            registeredValues.Add(valueName, value);
+        }
+
+        public void IntroduceRPC(string methodName, Delegate handler)
+        {
+            registeredRPCs.Add(methodName, handler);
+        }
+
+        public List<string> registeredPlugins { get; private set; } = new List<string>();
+        public Dictionary<string, object> registeredValues { get; private set; } = new Dictionary<string, object>();
+        public Dictionary<string, Delegate> registeredRPCs { get; private set; } = new Dictionary<string, Delegate>();
+    }
+}
--- Plugins/Diagnosis/DiagnosisPluginInitializer.cs
+++ Plugins/Diagnosis/DiagnosisPluginInitializer.cs
@@ -37,12 +37,21 @@ namespace DiagnosisPlugin
         public void Initialize()
         {
             Console.WriteLine("[Diagnosis] Diagnosis Website available at :8081/diagnosis/");
+            DiagnosisInterface.Instance.IntroducePlugin(this);
+            DiagnosisInterface.Instance.IntroduceValue("Registered Plugins", DiagnosisInterface.Instance.registeredPlugins);
+            DiagnosisInterface.Instance.IntroduceValue("Registered Values", DiagnosisInterface.Instance.registeredValues);
+            DiagnosisInterface.Instance.IntroduceRPC("hello", (Action)helloWorld);
             RequestDispatcher.Instance.RegisterHandler(new DiagnosisRequestHandler());
         }
 
         public void Shutdown()
         {
             Console.WriteLine("[Diagnosis] shutting down");
+        }
+
+        private void helloWorld()
+        {
+            Console.WriteLine("[DIAGNOSIS RPC] HELLO WORLD!");
         }
     }
 }
