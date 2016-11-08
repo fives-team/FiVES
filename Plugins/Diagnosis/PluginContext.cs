dcc7cf1f872e77466a9e32021a371c2d1fcee0cc
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2016-11-08 15:16:49 +0100

    Added Plugin Context object to Diagnosis Plugin

---
+++ Plugins/Diagnosis/PluginContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace DiagnosisPlugin
+{
+    public class PluginContext
+    {
+        IDiagnosablePlugin Plugin { get; private set; }
+
+        public PluginContext(IDiagnosablePlugin plugin)
+        {
+            this.Plugin = plugin;
+        }
+
+        public IPluginWidget GetPluginWidget()
+        {
+            return Plugin.Widget;
+        }
+
+        public void RegisterMethod(string methodName, Delegate handler)
+        {            
+            methodHandlers.Add(methodName, handler);
+        }
+
+        // Not sure if this should really be public
+        public bool CallMethod(string methodName, object[] parameters)
+        {
+            methodHandlers[methodName].DynamicInvoke(parameters);
+            // TODO: Was genau?! Bool on success
+            return true;
+        }
+
+        Dictionary<string, Delegate> methodHandlers = new Dictionary<string, Delegate>();
+    }
+}
