c13930a0823755a84c7689e26e945088159e09e7
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2016-11-08 15:16:07 +0100

    Added Interface for Plugin widgets

---
+++ Plugins/Diagnosis/IPluginWidget.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace DiagnosisPlugin
+{
+    abstract class IPluginWidget
+    {
+        XmlNode Render()
+        {
+            Root = new XmlDocument();
+            Root.AppendChild(RenderValues());
+            Root.AppendChild(RenderActions());
+            return Root;
+        }
+
+        virtual XmlNode RenderValues();
+        virtual XmlNode RenderActions();
+
+        XmlDocument Root;
+        HashSet<PluginValue> Values;
+        HashSet<PluginAction> Actions;
+    }
+}
