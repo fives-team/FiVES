805d030217f87d281f0cc9aff280770482cfcb23
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2016-11-08 15:16:33 +0100

    Added an extended Interface for Diagnosable Plugins

---
+++ Plugins/Diagnosis/IDiagnosablePlugin.cs
@@ -0,0 +1,15 @@
+﻿using FIVES;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace DiagnosisPlugin
+{
+    public interface IDiagnosablePlugin : IPluginInitializer
+    {
+        bool RegisterWidget();
+        IPluginWidget Widget { get; }
+    }
+}
