2e94b6734576e667d172beb3e958be909ae13c40
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2016-11-09 10:32:42 +0100

    Added XML Response Builder stub

---
+++ Plugins/Diagnosis/XmlResponseBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace DiagnosisPlugin
+{
+    class XmlResponseBuilder
+    {
+        private XmlDocument ResponseDocument;
+
+        public XmlResponseBuilder()
+        {
+        }
+
+        public XmlDocument RenderResponse()
+        {
+            RenderBaseInfo();
+            RenderPluginWidgets();
+            return ResponseDocument;
+        }
+
+        private void RenderBaseInfo()
+        {
+            // TODO: Base info direkt in XML injecten
+            RenderPluginWidgets();
+        }
+
+        private void RenderPluginWidgets()
+        {
+            foreach (PluginContext c in DiagnosisInterface.Instance.Contexts)
+            {
+                //Render (c.GetPluginWidget());
+            }
+        }
+
+        void ParseTemplateAsXml()
+        {
+            ResponseDocument = new XmlDocument();
+            ResponseDocument.Load("DiagnosisWebpage/dynamic/index.html");
+        }
+    }
+}
