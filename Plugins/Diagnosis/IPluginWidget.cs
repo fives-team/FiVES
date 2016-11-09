d9e0bd1e6484500d2a3012e9b3809c7fc572c04d
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2016-11-09 09:44:12 +0100

    Added method stubs for plugin widget rendering

--- Plugins/Diagnosis/IPluginWidget.cs
+++ Plugins/Diagnosis/IPluginWidget.cs
@@ -19,6 +19,21 @@ namespace DiagnosisPlugin
         public abstract XmlNode RenderValues();
         public abstract XmlNode RenderActions();
 
+        protected void renderWidgetContainer()
+        {
+
+        }
+
+        protected void renderTableRow()
+        {
+
+        }
+
+        protected void renderActionButton()
+        {
+
+        }
+
         XmlDocument Root;
         HashSet<PluginValue> Values;
         HashSet<PluginAction> Actions;
