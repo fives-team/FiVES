5c4f132dec3f0273a8da0beecd0b74e7eca1b113
Michael Guldner <migu01@dfki.de>
2017-04-24 09:25:09 +0200

    added a string-extension to remove a substring from the tail

---
+++ Plugins/SIXstrichLD/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIXstrichLDPlugin
+{
+    static class StringExtensions
+    {
+        public static string RemoveFromTail(this string s, string tailString)
+        {
+            if (s.EndsWith(tailString))
+            {
+                return s.Substring(0, s.Length - tailString.Length);
+            }
+            else
+            {
+                return s;
+            }
+        }
+    }
+}
