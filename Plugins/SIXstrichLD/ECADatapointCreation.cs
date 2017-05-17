c251895c0f5ba109563b61fa6d2c8ac8a03e9583
Michael Guldner <migu01@dfki.de>
2017-05-17 15:22:57 +0200

    create an observable for Attribute Datapoints so they can be subscribed onto a subscribed client will receive UpdateInfos

--- Plugins/SIXstrichLD/ECADatapointCreation.cs
+++ Plugins/SIXstrichLD/ECADatapointCreation.cs
@@ -5,6 +5,8 @@ using SIXCore.Serializers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -64,12 +66,38 @@ namespace SIXstrichLDPlugin
 
         public static void createAttributeDatapoint(this Server server, Uri componentUri, FIVES.Attribute attribute)
         {
+            var component = attribute.ParentComponent;
+            var entity = component.ContainingEntity;
             var attributeDefinition = attribute.Definition;
             var attributeUri = new Uri(componentUri.OriginalString + "/" + attributeDefinition.Name);
             var datapoint = server.CreateServerDatapoint(
                 attributeUri, new AttributeDatapointAdapter<UpdateInfo>(new JsonSerialization(), attribute)
             );
+            var observable = entity.getEntityObservable()
+                .Select(args => args.EventArgs)
+                .Where(args => args.Component.Name == component.Name && args.AttributeName == attribute.Definition.Name)
+                .Select(argsToUpdateInfo);
+                // TODO: use completeOnEntityRemoval from INVERSIV
+            datapoint.Subscribe(observable);
             Console.WriteLine("created A datapoint: " + attributeUri);
+        }
+
+        private static IObservable<System.Reactive.EventPattern<ChangedAttributeEventArgs>> getEntityObservable(this Entity entity)
+        {
+            return Observable.FromEventPattern<ChangedAttributeEventArgs>(
+                handler => entity.ChangedAttribute += handler,
+                handler => entity.ChangedAttribute -= handler
+            );
+        }
+
+        private static UpdateInfo argsToUpdateInfo(ChangedAttributeEventArgs args)
+        {
+            var updateInfo = new UpdateInfo();
+            updateInfo.attributeName = args.AttributeName;
+            updateInfo.componentName = args.Component.Name;
+            updateInfo.entityGuid = args.Component.ContainingEntity.Guid.ToString();
+            updateInfo.value = args.NewValue;
+            return updateInfo;
         }
     }
 }
