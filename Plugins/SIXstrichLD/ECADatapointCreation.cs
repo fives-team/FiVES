bba44ee7417b58fc38c23c51e3d17171fcdea1ef
Michael Guldner <migu01@dfki.de>
2017-05-22 10:49:12 +0200

    cleanup: removed unused imports

--- Plugins/SIXstrichLD/ECADatapointCreation.cs
+++ Plugins/SIXstrichLD/ECADatapointCreation.cs
@@ -3,13 +3,9 @@ using FIVES;
 using SIX;
 using SIXCore.Serializers;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
-using System.Reactive.Subjects;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SIXstrichLDPlugin
 {
