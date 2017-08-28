dd477fb98e6810e6b7c65066e57391329fad9f7c
Michael Guldner <migu01@dfki.de>
2017-08-28 11:18:17 +0200

    added summary for RemoveFromTail String Extension

--- Plugins/SIXstrichLD/StringExtensions.cs
+++ Plugins/SIXstrichLD/StringExtensions.cs
@@ -2,6 +2,12 @@
 {
     static class StringExtensions
     {
+        /// <summary>
+        /// Remove a substring at the end of a string
+        /// </summary>
+        /// <param name="s">string to remove from</param>
+        /// <param name="tailString">substring which shall be removed</param>
+        /// <returns>trimmed string or unchanged string if the substring does not exist at the end of the string</returns>
         public static string RemoveFromTail(this string s, string tailString)
         {
             if (s.EndsWith(tailString))
